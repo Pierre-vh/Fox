617eb4743752b32a48b53fd4fc8f870bf1923794
MyPix <pierre.vanhoutryve@gmail.com>
2017-12-12 23:00:43 +0100

    Removed old comments

--- Moonshot/main.cpp
+++ Moonshot/main.cpp
@@ -2,12 +2,6 @@
 
 #include "tests\testers\ExprTester.h"
 #include "src\Moonshot\Common\Errors\Errors.h"
-
-// TODO LIST
-// Make more detailed error message with a more robust error detection system.
-
-//TODO : BETA
-// Remove the enum TT_TYPE competly. Use variant indexes instead.
 int main()
 {
 	auto &err = Moonshot::Errors::getInstance();
