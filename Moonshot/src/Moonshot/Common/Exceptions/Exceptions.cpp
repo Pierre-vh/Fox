85af4543a7c409aedbd4146a8d662e2161953b8d
MyPix <pierre.vanhoutryve@gmail.com>
2018-01-02 00:07:23 +0100

    Forgot file header in Exceptions.cpp

--- Moonshot/src/Moonshot/Common/Exceptions/Exceptions.cpp
+++ Moonshot/src/Moonshot/Common/Exceptions/Exceptions.cpp
@@ -1,6 +1,15 @@
+////------------------------------------------------------////
+// This file is a part of The Moonshot Project.				
+// See LICENSE.txt for license info.						
+// File : Exceptions.cpp											
+// Author : Pierre van Houtryve								
+////------------------------------------------------------//// 
+//			SEE HEADER FILE FOR MORE INFORMATION			
+////------------------------------------------------------////
+
 #include "Exceptions.h"
 
-using namespace Moonshot;
+using namespace Moonshot::Exceptions;
 
 lexer_critical_error::lexer_critical_error(const std::string & msg) 
 {
