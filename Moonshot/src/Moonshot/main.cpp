172f1a66c6230c6504cbaf1f4a14256b7418f66d
Pierre van Houtryve <pierre.vanhoutryve@outlook.com>
2018-02-05 17:23:26 +0100

    updated main

--- Moonshot/src/Moonshot/main.cpp
+++ Moonshot/src/Moonshot/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
+#include <Windows.h>
 #include "Tests/Manager/TestManager.h"
-//#include <Windows.h>
-
 using namespace Moonshot;
 /*
 	ROADMAP (sort of):
@@ -30,32 +29,16 @@ using namespace Moonshot;
 			Generally, find ways to simplify the ast as much as possible.
 */
 /*
-	About constant folding, I think it's better to do it at the IR level instead of at the AST level. Less messing with pointers and stuff. But it's simpler at the ast level, so I don't know.
-	Else, here's how to do it
-	-> Constant folding
-		-> How to change the ast : Create a typedVisitor<std::unique_ptr<ASTExpr>>
-		FOR BINARY EXPR (node.left_ && node.right_)
-			->	In the expr function i'll need to call visit on both child at some point. If the result of one of the visit is a valid pointer (they have been changed)
-			change the left_ or right_ child to the new ones. DONT FORGET TO FREE THE OLD ONES.
-			-> Check if left_ and right_ are literals
-				-> true: calculate the expression and return a literal with the result.
-				-> false: return nullptr
-		FOR UNARY EXPR
-			Same stuff, except I'll only care about left_
-		-> make getValue private.
-		-> updateAST function that takes a unique ptr<astexpr> as argument, if the last value (value_) is a valid pointer, change the argument to that pointer, else, don't change it.
-		
-		To make the optimization on the whole program, in any node that contains an expression :
-			-> call visit on the node
-			-> If the result of the visit is nullptr, ignore it, if it's not, std::move the result to the node's expression pointer and free the old expression.
-*/
-/*
-	TO DO : Add other expression tests with another criteria. (Current expression tests checks if the condition evaluates to true, add other ones to analyze expressions better.) (One test for each operator)
+	TO DO: Implement UTF8 Properly in the lexer.
+		Add a encoding enum in context
+		Add a factory in Context : createStringManipulator(); which returns a polymorphic std::unique_ptr<IStringManipulator>, depending on the current encoding
+		Change the lexer & token accordingly.
 */
 int main()
 {
-	// Currently there's a problem : the windows console stops displaying when you try to print UTF8 chars. find a solution ! :(
-	//SetConsoleOutputCP(65001);
+
+	SetConsoleOutputCP(CP_UTF8);
+	setvbuf(stdout, nullptr, _IOFBF, 1000);
 
 	//std::ios_base::sync_with_stdio(false); // We don't use printf, so we don't need to sync with stdio (CppCoreGuidelines SL.io.10)
 	Context context;
