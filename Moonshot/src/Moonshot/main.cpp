10ca1ebc7c7ad110b9ae10943136d5f99a8eabc7
Pierre van Houtryve <pierre.vanhoutryve@outlook.com>
2018-03-10 17:59:38 +0100

    Updated notes

--- Moonshot/src/Moonshot/main.cpp
+++ Moonshot/src/Moonshot/main.cpp
@@ -12,18 +12,20 @@ using namespace Moonshot;
 			- Import
 			- DeclContexts (Symbols table and friends)
 
-		Rework the visitor pattern, add ASTWalker, ASTVisitor, ASTTraversal. (use a clang/swift-like design pattern)
 		While doing so, refactor the whole project.
 			Delete /eval/ folder. The code contained in that is complete trash.
 			Delete the typecheck class in prevision of upcoming rewrite.
 			Delete /datamap/, it's filth.
 			Take a peek at everything inside /Common/Types to rename stuff that needs it, make names more explicit. On top of my head:
-				FoxValue can be deleted altogether, and just use different ASTLiteral nodes.
+				FoxValue can be deleted altogether, and just use different ASTLiteral nodes. 
 				Look at everything that's not needed anymore an throw it in the trash too.
 			Also, take a peek a the general Option system. Maybe let it use .def files too.
 
 			After that, the project will be purged and can start again on healthier grounds.
+			I'm also thinking about switching to a CMake build system and start coding on Linux a bit more.
+			This will make the project better in the long run I think (more people open to contribute)
 
+		Rework the visitor pattern, add ASTWalker, ASTVisitor, ASTTraversal. (use a clang/swift-like design pattern)
 		Rewrite the whole test system, possibily by using .def files and a whole macro metaprogramming system.
 			- Separate tests in categories : lexer, parser, semantics, ...
 			- Add more control over tests individually: should it print the AST, should it just print "PASSED/FAILED", show them in condensed form.
