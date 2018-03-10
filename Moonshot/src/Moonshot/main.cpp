5d4095bc87d23edc9beccbbde46d4622d87f5f8a
Pierre van Houtryve <pierre.vanhoutryve@outlook.com>
2018-03-10 18:28:57 +0100

    Update

--- Moonshot/src/Moonshot/main.cpp
+++ Moonshot/src/Moonshot/main.cpp
@@ -12,7 +12,8 @@ using namespace Moonshot;
 			- Import
 			- DeclContexts (Symbols table and friends)
 
-		While doing so, refactor the whole project.
+		Rework the visitor pattern and adapt Dumper to support it. 
+		Refactor the whole project.
 			Delete /eval/ folder. The code contained in that is complete trash.
 			Delete the typecheck class in prevision of upcoming rewrite.
 			Delete /datamap/, it's filth.
@@ -23,19 +24,24 @@ using namespace Moonshot;
 			It should be really easy to add new options and Retrieve options. Not a pain in the ass as it is currently.
 				Options should all have int values.
 				Options should be easy to add and retrieve.
+			In general, mark stuff that should be reworked and delete things that need to be purged.
 
-			After that, the project will be purged and can start again on healthier grounds.
+			After that, the project will be able to start again on healthier grounds. This shouldn't be a huge setback, I'm
+			just throwing stuff in the trash to avoid building on it. I prefer to rewrite 2 to 3k sloc now than 10 to 30 later.
+
 			I'm also thinking about switching to a CMake build system and start coding on Linux a bit more.
-			This will make the project better in the long run I think (more people open to contribute)
+			This will make the project better in the long run I think (more people open to contribute, easier to build the project,etc)
 
 		Rework the visitor pattern, add ASTWalker, ASTVisitor, ASTTraversal. (use a clang/swift-like design pattern)
-		Rewrite the whole test system, possibily by using .def files.
+		Rewrite the whole test system, possibily by using .def files to automate their creation. I'll need to think about that a lot!
 			- Separate tests in categories : lexer, parser, semantics, ...
-			- Add more control over tests individually: should it print the AST, should it just print "PASSED/FAILED", show them in condensed form.
+			- Add more control over tests individually: should it print the AST, should it just print "PASSED/FAILED", show them in condensed form, etc.
 
 		Recreate semantic analysis phase in a /sema/ folder
 			- Name Resolver
 			- Type Checker
+			- Other general AST Validation / Semantic checks.
+		After that, (it's very far from now!) start doing the IR gen phase.
 */
 
 int main()
