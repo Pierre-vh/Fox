3901022591d4226dceec3c3b5368c8a36c9fe356
Pierre van Houtryve <pierre.vanhoutryve@outlook.com>
2018-03-23 17:53:24 +0100

    Tests finally show up in Visual Studio.
    'bout time

--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -3,7 +3,5 @@
 
 int main(int argc, char **argv) {
 	::testing::InitGoogleTest(&argc, argv);
-	auto code = RUN_ALL_TESTS();
-	std::cin.get(); // Needed so the window persists.
-	return code;
+	return RUN_ALL_TESTS;
 }
