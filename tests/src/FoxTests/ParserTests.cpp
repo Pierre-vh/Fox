5b6a0f15f6f73e56c52cfee02769f338e17e93d6
Pierre van Houtryve <pierre.vanhoutryve@outlook.com>
2018-03-24 17:42:49 +0100

    Adding ParserTests

---
+++ tests/src/FoxTests/ParserTests.cpp
@@ -0,0 +1,11 @@
+////------------------------------------------------------////
+// This file is a part of The Moonshot Project.				
+// See LICENSE.txt for license info.						
+// File : ParserTests.cpp											
+// Author : Pierre van Houtryve								
+////------------------------------------------------------//// 
+//	(Unit) Tests for the Parser.
+////------------------------------------------------------////
+
+#include "gtest/gtest.h"
+
