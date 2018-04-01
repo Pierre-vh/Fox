9b06c7f7bc3a882c396393ebdb2a6e54d0e77efc
Pierre van Houtryve <pierre.vanhoutryve@outlook.com>
2018-04-01 22:38:26 +0200

    Final tests for the DiagEngine ! It's done !

--- tests/src/CommonTests/CommonTests.cpp
+++ tests/src/CommonTests/CommonTests.cpp
@@ -95,10 +95,10 @@ TEST(DiagnosticsTests, warnings)
 TEST(DiagnosticsTests, errors)
 {
 	auto diagEng = createDiagEngine();
-	auto diag = diagEng.report(DiagID::unittests_errtest);
+	auto diag = diagEng.report(DiagID::unittest_errtest);
 	EXPECT_EQ("Test error", diag.getDiagStr()) << "Diagnostic string did not match";
 	EXPECT_EQ(DiagSeverity::ERROR, diag.getDiagSeverity()) << "Diagnostic severity did not match";
-	EXPECT_EQ(DiagID::unittests_errtest, diag.getDiagID()) << "Diagnostic id did not match";
+	EXPECT_EQ(DiagID::unittest_errtest, diag.getDiagID()) << "Diagnostic id did not match";
 }
 
 TEST(DiagnosticsTests, fatals)
@@ -160,10 +160,10 @@ TEST(DiagnosticsTests, errLimit)
 	diagEng.setErrorLimit(1);
 	EXPECT_FALSE(diagEng.hasFatalErrorOccured()) << "DiagnosticsEngine reported that a fatal error occured, but it was never used to report errors!";
 
-	diagEng.report(DiagID::unittests_errtest).emit();
+	diagEng.report(DiagID::unittest_errtest).emit();
 	EXPECT_FALSE(diagEng.hasFatalErrorOccured()) << "The DiagnosticsEngine reported a fatal error after 1 error.";
 
-	auto diag = diagEng.report(DiagID::unittests_errtest);
+	auto diag = diagEng.report(DiagID::unittest_errtest);
 	EXPECT_EQ(diag.getDiagID(), DiagID::diagengine_maxErrCountExceeded) << "The report function did not return the expected diagnostic";
 	EXPECT_EQ(diag.getDiagSeverity(), DiagSeverity::FATAL) << "The report function did not return a fatal diagnostic";
 	EXPECT_EQ("Current error count exceeded the maximum thresold of 1.", diag.getDiagStr()) << "Incorrect diagstr";
@@ -208,25 +208,103 @@ TEST(DiagnosticsTests, dummyDiags)
 	EXPECT_EQ(DiagSeverity::IGNORE, diag.getDiagSeverity()) << "Diag's severity was supposed to be \"IGNORE\"";
 }
 
-/* 
-Tests to write 
-Note: most of theses are implicitely proven to be working by the tests above, but i
-prefer to write more tests, so in the future if the projects gets more complicated I can
-find where the issue is much more quickly!
-	DiagEngine:
-		- Check if flagmanager options are correctly applied by creating flagsmanager
-		with different configurations & creating DiagEngines with them and check the getters.
-		btw, check if resetAll works.
-		
-		- Tests for:
-			warningsAreErrors
-			errorsAreFatal
-			silenceWarnings
-			silenceNotes
-			silenceAllAfterFatalErrors
-			silenceAll
-			getNumWarnings & getNumErrors
+TEST(DiagnosticsTests, FlagsAreCorrectlyApplied)
+{
+	auto diagEng = createDiagEngine();
+	FlagsManager f1, f2;
+	f1.set(CommonFlag::diagengine_errorsAreFatal);
+	f2.set(CommonFlag::diagengine_silenceAll);
+	f1.set(CommonFlag::diagengine_silenceAllAfterFatalError);
+	f2.set(CommonFlag::diagengine_silenceNotes);
+	f1.set(CommonFlag::diagengine_silenceWarnings);
+	f2.set(CommonFlag::diagengine_warningsAreErrors);
+	
+	diagEng.setFlagsManager(&f1);
+	EXPECT_TRUE(diagEng.getErrorsAreFatal());
+	EXPECT_FALSE(diagEng.getSilenceAll());
+	EXPECT_TRUE(diagEng.getSilenceAllAfterFatalErrors());
+	EXPECT_FALSE(diagEng.getSilenceNotes());
+	EXPECT_TRUE(diagEng.getSilenceWarnings());
+	EXPECT_FALSE(diagEng.getWarningsAreErrors());
 
-	Then finally move on to SourceManager & SourceLoc. This will be a bit long to do, but not much longer
-	than the DiagEngine I hope.
-*/
+	diagEng.setFlagsManager(&f2);
+	EXPECT_FALSE(diagEng.getErrorsAreFatal());
+	EXPECT_TRUE(diagEng.getSilenceAll());
+	EXPECT_FALSE(diagEng.getSilenceAllAfterFatalErrors());
+	EXPECT_TRUE(diagEng.getSilenceNotes());
+	EXPECT_FALSE(diagEng.getSilenceWarnings());
+	EXPECT_TRUE(diagEng.getWarningsAreErrors());
+}
+
+TEST(DiagnosticsTests, SilencedWarnings)
+{
+	auto diagEng = createDiagEngine();
+	// Set flag
+	diagEng.setSilenceWarnings(true);
+	// Test.
+	auto diagWarn = diagEng.report(DiagID::unittest_warntest);
+	EXPECT_EQ(diagWarn.getDiagID(), DiagID::dummyDiag) << "Reported diagnostic wasn't a silenced diag";
+	diagWarn.emit();
+}
+
+TEST(DiagnosticsTests, SilencedNotes)
+{
+	auto diagEng = createDiagEngine();
+	// Set flag
+	diagEng.setSilenceNotes(true);
+	// Test.
+	auto diagNote = diagEng.report(DiagID::unittest_notetest);
+	EXPECT_EQ(diagNote.getDiagID(), DiagID::dummyDiag) << "Reported diagnostic wasn't a silenced diag";
+	diagNote.emit();
+}
+
+TEST(DiagnosticsTests, SilenceAllAfterFatal)
+{
+	auto diagEng = createDiagEngine();
+	// Set flag
+	diagEng.setSilenceAllAfterFatalErrors(true);
+	// Test emission of an error
+	diagEng.report(DiagID::unittest_errtest).emit();
+	ASSERT_EQ(diagEng.getNumErrors(), 1) << "Error wasn't recorded?";
+	
+	// Report a fatal error
+	diagEng.report(DiagID::unittest_fataltest).emit();
+	EXPECT_EQ(diagEng.getNumErrors(), 1) << "Fatal error was counted like a normal error?";
+	ASSERT_TRUE(diagEng.hasFatalErrorOccured()) << "Fatal error didn't count?";
+
+	// And try to emit another error
+	auto diagErrSilenced = diagEng.report(DiagID::unittest_errtest);
+	EXPECT_EQ(diagErrSilenced.getDiagID(), DiagID::dummyDiag) << "Diag was supposed to be silenced an thus this DiagID was supposed to be a Dummy diag.";
+	diagErrSilenced.emit();
+}
+
+TEST(DiagnosticsTests, SilenceAll)
+{
+	auto diagEng = createDiagEngine();
+	diagEng.setSilenceAll(true);
+	auto dg1 = diagEng.report(DiagID::unittest_errtest);
+	auto dg2 = diagEng.report(DiagID::unittest_warntest);
+	auto dg3 = diagEng.report(DiagID::unittest_fataltest);
+
+	EXPECT_EQ(dg1.getDiagID(), DiagID::dummyDiag) << "Diag was supposed to be silenced.";
+	EXPECT_EQ(dg2.getDiagID(), DiagID::dummyDiag) << "Diag was supposed to be silenced.";
+	EXPECT_EQ(dg3.getDiagID(), DiagID::dummyDiag) << "Diag was supposed to be silenced.";
+}
+
+TEST(DiagnosticsTests, WarningsAreErrors)
+{
+	auto diagEng = createDiagEngine();
+	diagEng.setWarningsAreErrors(true);
+	diagEng.report(DiagID::unittest_warntest).emit();
+	EXPECT_EQ(diagEng.getNumWarnings(), 0) << "Diag shouldn't have counted a normal warning";
+	EXPECT_EQ(diagEng.getNumErrors(), 1) << "Diag didn't count as an error.";
+}
+
+TEST(DiagnosticsTests, ErrorsAreFatal)
+{
+	auto diagEng = createDiagEngine();
+	diagEng.setErrorsAreFatal(true);
+	diagEng.report(DiagID::unittest_errtest).emit();
+	EXPECT_TRUE(diagEng.hasFatalErrorOccured()) << "Diag didn't count as a fatal error.";
+	EXPECT_EQ(diagEng.getNumErrors(), 0) << "This error was supposed to be fatal and thus count as a fatal error, not a normal error.";
+}
