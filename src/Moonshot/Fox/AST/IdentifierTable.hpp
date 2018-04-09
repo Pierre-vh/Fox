da10c5c0ba5071f08106b8955db1395e42f9b5db
Pierre van Houtryve <pierre.vanhoutryve@outlook.com>
2018-04-09 23:31:27 +0200

    Added extra comments

--- src/Moonshot/Fox/AST/IdentifierTable.hpp
+++ src/Moonshot/Fox/AST/IdentifierTable.hpp
@@ -5,11 +5,20 @@
 // Author : Pierre van Houtryve								
 ////------------------------------------------------------//// 
 // This file contains the IdentifierTable, which is a class
-// that manages a "table" of identifiers. Every lexed identifier
-// ends up here.
-// This class offers lookup function using strings.
+// that manages a "table" of identifiers. Every lexed user-defined 
+// identifier ends up here.
 //
-// Note: This structure is heavily inspired by CLang/Swift.
+// The main goal of this class is 2 fold.
+// 1 - Reduce memory usage a bit, every identifier is stored once, and then only pointers to it are stored.
+// 2 - Enable a centralized identifier table, which allows to attach specific information to an identifier.
+// 
+// This class offers lookup functions using Strings, and can return a reference or a pointer to a map entry.
+// 
+// Note: This structure is heavily inspired by CLang. Well, a lot here is inspired by this incredible compiler.
+// Of course, I adapt and simplify some stuff, but the core ideas come from CLang
+// 
+// Also, note that 0 lines of source code were copy-pasted from the source code of CLang. I'm only attempting to reproduce
+// what I understand that they're doing to increase the quality of this compiler and learn more about how CLang works.
 ////------------------------------------------------------////
 
 #include <map>
@@ -20,6 +29,7 @@ namespace Moonshot
 	class IdentifierInfo;
 
 	// Wrapper around a const_iterator of a map entry, used to safely access the .first element (the string)
+	// This is used to avoid storing a raw pointer or iterator.
 	// Based on a SO answer:  https://stackoverflow.com/a/516041
 	class StringPtrInMap
 	{
@@ -29,12 +39,13 @@ namespace Moonshot
 			StringPtrInMap(ItTy iter);
 
 			const std::string& get() const;
-			ItTy it_;
 
+			ItTy it_;
 	};
 
 	// A Class holding informations related to a identifier.
 	// Currently, it only lets us access the std::string. In the future, this might hold additional informations.
+	// That's the advantage of this class, I can add information to an identifier without breaking everything!
 	class IdentifierInfo
 	{
 		public:
@@ -57,7 +68,8 @@ namespace Moonshot
 			StringPtrInMap mapIter_;
 	};
 
-	// a map containing every identifier currently in use.
+	// A class that maps strings to IdentifierInfo.
+	// This contains every (user-defined) identifier currently in use.
 	class IdentifierTable
 	{
 		private:
