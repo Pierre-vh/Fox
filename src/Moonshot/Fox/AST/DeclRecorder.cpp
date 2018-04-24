206dafcc7afbf1f5340d8dc1dc093aae5bf8476f
Pierre VAN HOUTRYVE <pierre.vanhoutryve@gmail.com>
2018-04-24 11:11:11 +0200

    Adding DeclRecorder

---
+++ src/Moonshot/Fox/AST/DeclRecorder.cpp
@@ -0,0 +1,169 @@
+////------------------------------------------------------////
+// This file is a part of The Moonshot Project.				
+// See LICENSE.txt for license info.						
+// File : DeclRecorder.cpp											
+// Author : Pierre van Houtryve								
+////------------------------------------------------------//// 
+//			SEE HEADER FILE FOR MORE INFORMATION			
+////------------------------------------------------------////
+
+
+#include "DeclRecorder.hpp"
+#include "ASTDecl.hpp"
+
+#include "Moonshot/Fox/Basic/Exceptions.hpp"
+#include <cassert>
+
+using namespace Moonshot;
+
+DeclRecorder::DeclRecorder(DeclRecorder * upperDR) : parent_(upperDR)
+{
+
+}
+
+void DeclRecorder::recordDecl(ASTNamedDecl * decl)
+{
+	assert(decl && "Declaration cannot be null!");
+	namedDecls_.insert(std::make_pair(decl->getDeclName(), decl));
+}
+
+LookupResult DeclRecorder::restrictedLookup(IdentifierInfo * id) const
+{
+	auto it_range = namedDecls_.equal_range(id);
+	LookupResult lr;
+	for (auto it = it_range.first; it != it_range.second; it++)
+		lr.addResult(it->second);
+	return lr;
+}
+
+LookupResult DeclRecorder::fullLookup(IdentifierInfo * id) const
+{
+	auto this_lr = restrictedLookup(id);
+
+	if (parent_)
+	{
+		auto parent_lr = parent_->fullLookup(id);
+		this_lr.merge(parent_lr);
+	}
+		
+	return this_lr;
+}
+
+bool DeclRecorder::hasParentDeclRecorder() const
+{
+	return parent_;
+}
+
+DeclRecorder * DeclRecorder::getParentDeclRecorder()
+{
+	return parent_;
+}
+
+void DeclRecorder::setParentDeclRecorder(DeclRecorder * dr)
+{
+	parent_ = dr;
+}
+
+DeclRecorder::NamedDeclsMapIter DeclRecorder::recordedDecls_begin()
+{
+	return namedDecls_.begin();
+}
+
+DeclRecorder::NamedDeclsMapIter DeclRecorder::recordedDecls_end()
+{
+	return namedDecls_.begin();
+}
+
+DeclRecorder::NamedDeclsMapConstIter DeclRecorder::recordedDecls_begin() const
+{
+	return namedDecls_.end();
+}
+
+DeclRecorder::NamedDeclsMapConstIter DeclRecorder::recordedDecls_end() const
+{
+	return namedDecls_.end();
+}
+
+LookupResult::LookupResult()
+{
+	containsFuncDecl_ = false;
+	containsVarDecl_ = false;
+}
+
+// LookupResult
+bool LookupResult::isEmpty() const
+{
+	return !results_.size();
+}
+
+bool LookupResult::isUnique() const
+{
+	return (results_.size() == 1);
+}
+
+ASTNamedDecl * LookupResult::getResultIfUnique() const
+{
+	return (results_.size() == 1) ? results_[0] : nullptr;
+}
+
+bool LookupResult::containsFunctionDecls() const
+{
+	return containsFuncDecl_;
+}
+
+bool LookupResult::containsVarDecl() const
+{
+	return containsVarDecl_;
+}
+
+bool LookupResult::onlyContainsFunctionDecls() const
+{
+	for (auto it = results_.begin(); it != results_.end(); it++)
+	{
+		// Return false if one of the results can't be dynamic_cast to a ASTFunctionDecl*
+		if (!dynamic_cast<ASTFunctionDecl*>(*it))
+			return false;
+	}
+	// Only returns true if there's at least one result.
+	return (results_.size() != 0);
+}
+
+LookupResult::operator bool() const
+{
+	return !isEmpty();
+}
+
+void LookupResult::addResult(ASTNamedDecl * decl)
+{
+	if (results_.size())
+		assert((results_.back()->getDeclName() == decl->getDeclName()) && "A LookupResult can only contain NamedDecl that share the same identifier.");
+
+	if (dynamic_cast<ASTFunctionDecl*>(decl))
+		containsFuncDecl_ = true;
+	else if (dynamic_cast<ASTVarDecl*>(decl))
+		containsVarDecl_ = true;
+
+	results_.push_back(decl);
+}
+
+void LookupResult::clear()
+{
+	containsFuncDecl_ = false;
+	containsVarDecl_ = false;
+	results_.clear();
+}
+
+void LookupResult::merge(LookupResult & target)
+{
+	if(target.results_.size() != 0)
+		results_.insert(results_.end(), target.results_.begin(), target.results_.end());
+	
+	// update flags appropriately.
+	containsFuncDecl_	= containsFuncDecl_ || target.containsFuncDecl_;
+	containsVarDecl_	= containsVarDecl_ || target.containsVarDecl_;
+
+	// Clear target.
+	target.clear();
+}
+
+
---
+++ src/Moonshot/Fox/AST/DeclRecorder.hpp
@@ -0,0 +1,106 @@
+////------------------------------------------------------////
+// This file is a part of The Moonshot Project.				
+// See LICENSE.txt for license info.						
+// File : DeclRecorder.hpp											
+// Author : Pierre van Houtryve								
+////------------------------------------------------------//// 
+// DeclRecorder acts as a Declaration Recorder. While parsing, the 
+// parser "registers" every declaration in the most recent declaration recorder found.
+// DeclRecorder assists during name resolution to find members of a unit, or just to find if a
+// identifier is linked to one or more declaration in a specific place of the AST.
+////------------------------------------------------------////
+
+#include <map>
+#include <vector>
+
+namespace Moonshot
+{
+	class ASTDecl;
+	class ASTNamedDecl;
+	class IdentifierInfo;
+	class LookupResult;
+	class DeclRecorder
+	{
+		private:
+			using NamedDeclsMapTy = std::multimap<IdentifierInfo*, ASTNamedDecl*>;
+			using NamedDeclsMapIter = NamedDeclsMapTy::iterator;
+			using NamedDeclsMapConstIter = NamedDeclsMapTy::const_iterator;
+		public:
+			DeclRecorder(DeclRecorder* upperDR = nullptr);
+
+			// "Record" a declaration within this DeclRecorder
+			void recordDecl(ASTNamedDecl* decl);
+
+			// Searches for every NamedDecl whose identifier == id in this DeclRecorder
+			LookupResult restrictedLookup(IdentifierInfo *id) const;
+
+			// Performs a restrictedLookup on this DeclRecorder and recursively searches parent
+			// DeclRecorders.
+			LookupResult fullLookup(IdentifierInfo *id) const;
+
+			// Manage parent decl recorder
+			bool hasParentDeclRecorder() const;
+			DeclRecorder* getParentDeclRecorder();
+			void setParentDeclRecorder(DeclRecorder *dr);
+
+			NamedDeclsMapIter recordedDecls_begin();
+			NamedDeclsMapIter recordedDecls_end();
+
+			NamedDeclsMapConstIter recordedDecls_begin() const;
+			NamedDeclsMapConstIter recordedDecls_end() const;
+		private:
+			// Pointer to the Declaration Recorder "above" this one.
+			DeclRecorder * parent_ = nullptr;
+			NamedDeclsMapTy namedDecls_;
+	};
+
+	// A Class that encapsulates a lookup result.
+	// All NamedDecls stored here are assumed to have the same IdentifierInfo.
+	// an assertion in addResult ensures this.
+	class LookupResult
+	{
+		private:
+			using ResultVecTy = std::vector<ASTDecl*>;
+			using ResultVecIter = ResultVecTy::iterator;
+			using ResultVecConstIter = ResultVecTy::const_iterator;
+		public:
+			LookupResult();
+
+			// Returns false if this LookupResult is empty.
+			bool isEmpty() const;
+
+			// Returns true if this LookupResult contains only one result.
+			bool isUnique() const;
+
+			// If this LookupResult contains only one result, returns it, else, returns a nullptr.
+			ASTNamedDecl* getResultIfUnique() const;
+
+			// Returns true if this LookupResult contains at least one function declaration.
+			bool containsFunctionDecls() const;
+
+			// Returns true if this LookupResult contains only function declarations.
+			bool containsVarDecl() const;
+
+			// Iterates over this LookupResult's results and checks if every single one of them
+			// is a function declaration.
+				// Note: Since every NamedDecl has the same name in a LookupResult, if they're all functions, that means it's an "overload set"
+			bool onlyContainsFunctionDecls() const;
+
+			operator bool() const;
+		protected:
+			friend class DeclRecorder;
+
+			// Add another lookup result.
+			void addResult(ASTNamedDecl* decl);
+			// Clear this LookupResult
+			void clear();
+			// Copies all of the results from target into this lookupresult then clears the target.
+			void merge(LookupResult &target);
+		private:
+			std::vector<ASTNamedDecl*> results_;
+
+			// Theses flags are set to keep track of the "diversity" of named decls contained in this lookupresult.
+			bool containsFuncDecl_		: 1;	// Contains at least 1 func decl
+			bool containsVarDecl_		: 1;	// Contains at least 1 var decl
+	};
+}
