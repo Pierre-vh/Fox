ef632c9789d749cff5304bc8399868fa4baa962d
Pierre van Houtryve <pierre.vanhoutryve@outlook.com>
2018-06-29 15:04:28 +0200

    Redefining fox_unreachable

--- src/Fox/Common/Utils.hpp
+++ src/Fox/Common/Utils.hpp
@@ -9,6 +9,7 @@
 
 #pragma once
 
+#include <type_traits>
 #include <cassert>
 
 namespace fox
@@ -19,10 +20,13 @@ namespace fox
 	{
 		return static_cast<typename std::underlying_type<Enumeration>::type>(value);
 	}
+
+	[[ noreturn ]]
+	void _fox_unreachable_internal(const char* message, const char* file, const unsigned& line);
 }
 
 
 // Quick macro to mark some piece of code as unreachable.
 #ifndef fox_unreachable
-	#define fox_unreachable(msg) assert(false && msg)
+	#define fox_unreachable(msg) ::fox::_fox_unreachable_internal(msg,__FILE__,__LINE__)
 #endif
