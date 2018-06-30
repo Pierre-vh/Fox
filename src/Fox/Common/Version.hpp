6659ad880b61fd23805d5c5d2ead656894198a14
Pierre van Houtryve <pierre.vanhoutryve@outlook.com>
2018-06-30 17:43:52 +0200

    Update comments

--- src/Fox/Common/Version.hpp
+++ src/Fox/Common/Version.hpp
@@ -5,7 +5,7 @@
 // Author : Pierre van Houtryve								
 ////------------------------------------------------------//// 
 // This file contains the version Major/Minor/Patch and other information
-// About the current development state of the Moonshot Project
+// About the current version of the Moonshot Project
 ////------------------------------------------------------////
 
 #define MOONSHOT_UTIL_TOSTR_HELPER(X) #X
@@ -17,21 +17,19 @@
 #define MOONSHOT_VER_MINOR 2
 #define MOONSHOT_VER_PATCH 0
 
-// Possible version types:
-	// inDev : inDevelopement, nothing is complete/working as intended
-	// Alpha : It's taking shape, but still not quite usable
-	// Beta  : Polishing, "internal changes" (changes not visible by the user, like solving perf problems, polishing the API, bugfixing)
-	// Release : Usable release
-#define MOONSHOT_VER_TYPE "inDev"	
+#define MOONSHOT_DEV_PHASE "inDev"	
+
+// Current Version : 0 -> Development of the interpreter.
+
+// Description of each "development" phase
+	// inDev : Nothing is complete/working as intended	(Make it work)
+	// Alpha : The interpreter is working and can run basic Fox code. This phase is mostly improving the existing code :
+	//		   Writing more tests, Refactoring, DRYing, etc. At the end of this phase, the code should be clean and pleasant to read. (Make it right)
+	// Beta  : Optimization-oriented development phase. This include refactoring/rewriting slow code, profiling, etc (Make it fast)	
+	//			
+	// Release : The interpreter is considered good enough to be usable by other people. It's decently fast, nice to work with. Patches in a release version are mostly
+	//			 tweaking/bugfixes.
+
 
 #define MOONSHOT_VERSION MOONSHOT_UTIL_TOSTR(MOONSHOT_VER_MAJOR) "." MOONSHOT_UTIL_TOSTR(MOONSHOT_VER_MINOR) "." MOONSHOT_UTIL_TOSTR(MOONSHOT_VER_PATCH)
-#define MOONSHOT_VERSION_COMPLETE MOONSHOT_VERSION " (" MOONSHOT_VER_TYPE ")"
-
-
-// Next planned version number changes
-	// Next Major incrementation : Badger and Fox are fully operational, ready for public use. (Estimation : Q4 2018/Q1 2019 with some luck)
-	// Next Minor incrementation 
-		// "inDev"
-			// + Diagnostics rework is complete.
-			// + Semantic Analysis Done
-	// Next Patch Incrementation : Not planned until 1.0.0 since no patch's needed. It's all indev.
+#define MOONSHOT_VERSION_COMPLETE MOONSHOT_VERSION " (" MOONSHOT_DEV_PHASE ")"
