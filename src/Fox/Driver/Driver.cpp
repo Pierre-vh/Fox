876a78f2e47c9c454ebf7b6a4311b20fb0e0f34f
Pierre van Houtryve <pierre.vanhoutryve@outlook.com>
2018-07-31 16:31:11 +0200

    Adding Allocator Dump

--- src/Fox/Driver/Driver.cpp
+++ src/Fox/Driver/Driver.cpp
@@ -24,17 +24,17 @@ bool Driver::processFile(std::ostream& out, const std::string& filepath)
 	SourceManager srcMgr;
 	DiagnosticEngine dg(srcMgr);
 	// Create a ASTContext
-	std::unique_ptr<ASTContext> astCtxt = std::make_unique<ASTContext>();
+	ASTContext astCtxt;
 
 	auto fid = srcMgr.loadFromFile(filepath);
 	if (!fid)
 	{
-		std::cout << "Could not open file \"" << filepath << "\"\n";
+		out << "Could not open file \"" << filepath << "\"\n";
 		return false;
 	}
 	auto t0 = std::chrono::high_resolution_clock::now();
 
-	Lexer lex(dg,srcMgr,*astCtxt);
+	Lexer lex(dg, srcMgr, astCtxt);
 	lex.lexFile(fid);
 
 	auto t1 = std::chrono::high_resolution_clock::now();
@@ -47,9 +47,9 @@ bool Driver::processFile(std::ostream& out, const std::string& filepath)
 		return false;
 	}
 
-	Parser psr(dg,srcMgr,*astCtxt,lex.getTokenVector());
+	Parser psr(dg,srcMgr, astCtxt,lex.getTokenVector());
 	// Todo: extract the name of the file and use that instead of "TestUnit"
-	auto unit = psr.parseUnit(fid,astCtxt->identifiers.getUniqueIdentifierInfo("TestUnit"), /* is main unit */ true);
+	auto unit = psr.parseUnit(fid, astCtxt.identifiers.getUniqueIdentifierInfo("TestUnit"), /* is main unit */ true);
 
 	if (!unit)
 	{
@@ -57,29 +57,30 @@ bool Driver::processFile(std::ostream& out, const std::string& filepath)
 		return false;
 	}
 
-	out << "\nSuccess ! Dump:\n";
-	
+	out << "\nSuccess ! Dumping allocator:\n";
+	astCtxt.getAllocator().dump(out);
+	out << "\nDumping main unit:\n";
+
 	auto t2 = std::chrono::high_resolution_clock::now();
 	auto parse_micro = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
 	auto parse_milli = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
 	
 	ASTDumper dumper(srcMgr, std::cout, 1);
-	dumper.visit(astCtxt->getMainUnit());
+	dumper.visit(astCtxt.getMainUnit());
 
 	auto t3 = std::chrono::high_resolution_clock::now();
 	auto dump_micro = std::chrono::duration_cast<std::chrono::microseconds>(t3 - t2).count();
 	auto dump_milli = std::chrono::duration_cast<std::chrono::milliseconds>(t3 - t2).count();
 
-	astCtxt.reset();
+	astCtxt.getAllocator().reset();
 
 	auto t4 = std::chrono::high_resolution_clock::now();
 	auto release_micro = std::chrono::duration_cast<std::chrono::microseconds>(t4 - t3).count();
 	auto release_milli = std::chrono::duration_cast<std::chrono::milliseconds>(t4 - t3).count();
 
-	std::cout << "\nLexing time :\n\t" << lex_micro << " microseconds\n\t" << lex_milli << " milliseconds\n";
-	std::cout << "\nParsing time :\n\t" << parse_micro << " microseconds\n\t" << parse_milli << " milliseconds\n";
-	std::cout << "\nAST dump time :\n\t" << dump_micro << " microseconds\n\t" << dump_milli << " milliseconds\n";
-	std::cout << "\nAST release time :\n\t" << release_micro << " microseconds\n\t" << release_milli << " milliseconds\n";
-
+	out << "\nLexing time :\n\t" << lex_micro << " microseconds\n\t" << lex_milli << " milliseconds\n";
+	out << "\nParsing time :\n\t" << parse_micro << " microseconds\n\t" << parse_milli << " milliseconds\n";
+	out << "\nAST dump time :\n\t" << dump_micro << " microseconds\n\t" << dump_milli << " milliseconds\n";
+	out << "\nAST release time :\n\t" << release_micro << " microseconds\n\t" << release_milli << " milliseconds\n";
 	return true;
 }
