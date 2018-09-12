2ee3835f0fb7957afc95d48bfea88aba8802e0b8
Pierre van Houtryve <pierre.vanhoutryve@outlook.com>
2018-09-12 19:58:44 +0200

    Initial Commit for Sema

---
+++ src/Fox/Sema/Sema.hpp
@@ -0,0 +1,14 @@
+////------------------------------------------------------////
+// This file is a part of The Moonshot Project.				
+// See LICENSE.txt for license info.						
+// File : Sema.hpp											
+// Author : Pierre van Houtryve								
+////------------------------------------------------------//// 
+// Contains the Sema class, which is used to perform 
+// most of the semantic analysis of a Fox AST.
+////------------------------------------------------------//// 
+
+namespace fox
+{
+
+}
