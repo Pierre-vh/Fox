f8f1a3f2ddb2cc96c67440c7e3567801864697d1
Pierre van Houtryve <pierre.vanhoutryve@outlook.com>
2018-09-13 16:22:39 +0200

    Adding SemaExprs

---
+++ src/Fox/Sema/SemaExprs.cpp
@@ -0,0 +1,16 @@
+////------------------------------------------------------////
+// This file is a part of The Moonshot Project.				
+// See LICENSE.txt for license info.						
+// File : SemaExprs.cpp										
+// Author : Pierre van Houtryve								
+////------------------------------------------------------//// 
+//			SEE HEADER FILE FOR MORE INFORMATION			
+//	This file implements Sema methods related to Exprs
+////------------------------------------------------------////
+
+#include "Sema.hpp"
+#include "Fox/AST/Expr.hpp"
+#include "Fox/Common/Errors.hpp"
+#include "Fox/Common/LLVM.hpp"
+
+using namespace fox;
