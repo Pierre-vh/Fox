866e137af82a021146f6b7eef7e76e7f4257af8e
Pierre-vh <pierre.vanhoutryve@outlook.com>
2018-09-21 08:53:09 +0200

    Beginning ASTWalker implementation

--- includes/Fox/AST/ASTWalker.hpp
+++ includes/Fox/AST/ASTWalker.hpp
@@ -7,7 +7,6 @@
 // This file contains the ASTWalker class, which is used to
 // "walk" the ast in a pre/post order fashion, automatically 
 // replacing nodes if needed.
-//
 ////------------------------------------------------------////
 
 #include <tuple>
@@ -24,16 +23,63 @@ namespace fox
 			Stmt* walk(Stmt* stmt);
 			Type* walk(Type* type);
 
-			virtual std::pair<bool, Expr*> handleExprPre(Expr* expr);
+			// Called when first visiting an expression before visiting it's
+			// children. 
+			// The first element of the return pair is the node that should
+			// take this node's place, if it's nullptr, the traversal is terminated.
+			// The second element is a boolean indicating if we should visit this node's
+			// children.
+			virtual std::pair<Expr*, bool> handleExprPre(Expr* expr);
+
+			// Called after visiting an expression's children.
+			// If the return value is null, the traversal is terminated, otherwise
+			// the walked node is replaced by the returned node.
+			// The default implementation returns it's argument.
 			virtual Expr* handleExprPost(Expr* expr);
 
-			virtual std::pair<bool, Stmt*> handleStmtPre(Stmt* stmt);
+			// Called when first visiting an statement before visiting it's
+			// children. 
+			// The first element of the return pair is the node that should
+			// take this node's place, if it's nullptr, the traversal is terminated.
+			// The second element is a boolean indicating if we should visit this node's
+			// children.
+			virtual std::pair<Stmt*, bool> handleStmtPre(Stmt* stmt);
+
+
+			// Called after visiting an statement's children.
+			// If the return value is null, the traversal is terminated, otherwise
+			// the walked node is replaced by the returned node.
+			// The default implementation returns it's argument.
 			virtual Stmt* handleStmtPost(Stmt* stmt);
 
-			virtual std::pair<bool, Decl*> handleDeclPre(Decl* decl);
+			// Called when first visiting an declaration before visiting it's
+			// children. 
+			// The first element of the return pair is the node that should
+			// take this node's place, if it's nullptr, the traversal is terminated.
+			// The second element is a boolean indicating if we should visit this node's
+			// children.
+			virtual std::pair<Decl*, bool> handleDeclPre(Decl* decl);
+
+
+			// Called after visiting an declaration's children.
+			// If the return value is null, the traversal is terminated, otherwise
+			// the walked node is replaced by the returned node.
+			// The default implementation returns it's argument.
 			virtual Decl* handleDeclPost(Decl* decl);
 
-			virtual std::pair<bool, Type*> handleTypePre(Type* type);
+			// Called when first visiting an type before visiting it's
+			// children. 
+			// The first element of the return pair is the node that should
+			// take this node's place, if it's nullptr, the traversal is terminated.
+			// The second element is a boolean indicating if we should visit this node's
+			// children.
+			virtual std::pair<Type*, bool> handleTypePre(Type* type);
+
+
+			// Called after visiting an type's children.
+			// If the return value is null, the traversal is terminated, otherwise
+			// the walked node is replaced by the returned node.
+			// The default implementation returns it's argument.
 			virtual Type* handleTypePost(Type* type);
 	};
 }
--- lib/AST/ASTWalker.cpp
+++ lib/AST/ASTWalker.cpp
@@ -16,18 +16,103 @@ using namespace fox;
 namespace
 {
 	class Traverse : public ASTVisitor<Traverse, Decl*, Expr*, Stmt*, Type*>
-	{
-		private:
-			// Implementation of ASTWalker
-				// visit methods must return a node, which is the current node or
-				// or the replacement. That node cannot be null.
-			
+	{		
 		public:
 			Traverse(ASTWalker& walker):
 				walker_(walker)
 			{}
 
+			Expr* visitParensExpr(ParensExpr* expr)
+			{
+				if (Expr* node = doIt(expr->getExpr()))
+				{
+					expr->setExpr(node);
+					return expr;
+				}
+				return nullptr;
+			}
+
 		private:
+			// doIt method for expression: handles call to the walker &
+			// requests visitation of the children of a given node.
+			Expr* doIt(Expr* expr)
+			{
+				// Let the walker handle the pre visitation stuff.
+				auto rtr = walker_.handleExprPre(expr);
+
+				// Return if we have a nullptr or if we're instructed
+				// to not visit the children.
+				if (!rtr.first || !rtr.second)
+					return rtr.first;
+
+				// visit the node's childre, and if the traversal wasn't aborted,
+				// let the walker handle post visitation stuff.
+				if (expr = visit(rtr.first))
+					expr = walker_.handleExprPost(expr);
+
+				return expr;
+			}
+
+			// doIt method for declarations: handles call to the walker &
+			// requests visitation of the children of a given node.
+			Decl* doIt(Decl* expr)
+			{
+				// Let the walker handle the pre visitation stuff.
+				auto rtr = walker_.handleDeclPre(expr);
+
+				// Return if we have a nullptr or if we're instructed
+				// to not visit the children.
+				if (!rtr.first || !rtr.second)
+					return rtr.first;
+
+				// visit the node's childre, and if the traversal wasn't aborted,
+				// let the walker handle post visitation stuff.
+				if (expr = visit(rtr.first))
+					expr = walker_.handleDeclPost(expr);
+
+				return expr;
+			}
+
+			// doIt method for statements: handles call to the walker &
+			// requests visitation of the children of a given node.
+			Stmt* doIt(Stmt* expr)
+			{
+				// Let the walker handle the pre visitation stuff.
+				auto rtr = walker_.handleStmtPre(expr);
+
+				// Return if we have a nullptr or if we're instructed
+				// to not visit the children.
+				if (!rtr.first || !rtr.second)
+					return rtr.first;
+
+				// visit the node's childre, and if the traversal wasn't aborted,
+				// let the walker handle post visitation stuff.
+				if (expr = visit(rtr.first))
+					expr = walker_.handleStmtPost(expr);
+
+				return expr;
+			}
+
+			// doIt method for types: handles call to the walker &
+			// requests visitation of the children of a given node.
+			Type* doIt(Type* expr)
+			{
+				// Let the walker handle the pre visitation stuff.
+				auto rtr = walker_.handleTypePre(expr);
+
+				// Return if we have a nullptr or if we're instructed
+				// to not visit the children.
+				if (!rtr.first || !rtr.second)
+					return rtr.first;
+
+				// visit the node's childre, and if the traversal wasn't aborted,
+				// let the walker handle post visitation stuff.
+				if (expr = visit(rtr.first))
+					expr = walker_.handleTypePost(expr);
+
+				return expr;
+			}
+
 			ASTWalker& walker_;
 	};
 } // End anonymous namespace
@@ -64,9 +149,9 @@ Type* ASTWalker::walk(Type* type)
 	return Traverse(*this).visit(type);
 }
 
-std::pair<bool, Expr*> ASTWalker::handleExprPre(Expr* expr)
+std::pair<Expr*, bool> ASTWalker::handleExprPre(Expr* expr)
 {
-	return { true, expr };
+	return { expr, true };
 }
 
 Expr* ASTWalker::handleExprPost(Expr* expr)
@@ -74,9 +159,9 @@ Expr* ASTWalker::handleExprPost(Expr* expr)
 	return expr;
 }
 
-std::pair<bool, Stmt*> ASTWalker::handleStmtPre(Stmt* stmt)
+std::pair<Stmt*, bool> ASTWalker::handleStmtPre(Stmt* stmt)
 {
-	return { true, stmt };
+	return { stmt, true };
 }
 
 Stmt* ASTWalker::handleStmtPost(Stmt* stmt)
@@ -84,9 +169,9 @@ Stmt* ASTWalker::handleStmtPost(Stmt* stmt)
 	return stmt;
 }
 
-std::pair<bool, Decl*> ASTWalker::handleDeclPre(Decl* decl)
+std::pair<Decl*, bool> ASTWalker::handleDeclPre(Decl* decl)
 {
-	return { true, decl };
+	return { decl, true };
 }
 
 Decl* ASTWalker::handleDeclPost(Decl* decl)
@@ -94,9 +179,9 @@ Decl* ASTWalker::handleDeclPost(Decl* decl)
 	return decl;
 }
 
-std::pair<bool, Type*> ASTWalker::handleTypePre(Type* type)
+std::pair<Type*, bool> ASTWalker::handleTypePre(Type* type)
 {
-	return { true, type };
+	return { type, true };
 }
 
 Type* ASTWalker::handleTypePost(Type* type)
