9f95908daff6dd8db4b897ea46b12ea2bb29c0bb
Pierre van Houtryve <pierre.vanhoutryve@outlook.com>
2018-09-24 18:39:53 +0200

    Adding ctor, ASTContext& member + getHighestRankingType

--- includes/Fox/Sema/Sema.hpp
+++ includes/Fox/Sema/Sema.hpp
@@ -8,22 +8,26 @@
 // most of the semantic analysis of a Fox AST.
 ////------------------------------------------------------//// 
 
+#pragma once
+
 #include <cstdint>
 #include "Fox/AST/ASTNode.hpp"
 #include "Fox/AST/ASTFwdDecl.hpp"
-#include "Fox/Common/ResultObject.hpp"
+#include "Fox/AST/ASTContext.hpp"
 
 namespace fox
 {
 	class Sema
 	{
 		public:
+			Sema(ASTContext& ctxt);
+
 			// Typedefs
 			using IntegralRankTy = std::uint8_t;
 
 			// The unification algorithms for types of the same subtypes.
 			//
-			// SemaType with no subs. + Any type -> True
+			// SemaType with no subs. + Any type -> True, sets appropriate subst
 			// Any Type & Any Type -> returns compareSubtype(a,b)
 			// False in all other cases.
 			static bool unifySubtype(Type* a, Type* b);
@@ -32,10 +36,15 @@ namespace fox
 			// share the same "subtype", false otherwise.
 			static bool compareSubtypes(Type* a, Type* b);
 
+			// Given 2 types of the same subtype, return the highest ranking type.
+			// Returns nullptr if the 2 types don't share the same subtype.
+			static Type* getHighestRankingType(Type* a, Type* b);
+
 			// This method returns the integral rank that a given type has.
 			// type must not be null and must point to a arithmetic type.
 			static IntegralRankTy getIntegralRank(PrimitiveType* type);
 
 		private:
+			ASTContext &ctxt_;
 	};
 }
