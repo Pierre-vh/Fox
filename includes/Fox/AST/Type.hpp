4c6102313fff686409f80490cb6d3326e0965abd
Pierre van Houtryve <pierre.vanhoutryve@outlook.com>
2018-09-26 15:26:22 +0200

    Initial commit for Type.hpp/.cpp

---
+++ includes/Fox/AST/Type.hpp
@@ -0,0 +1,48 @@
+////------------------------------------------------------////
+// This file is a part of The Moonshot Project.				
+// See LICENSE.txt for license info.						
+// File : Type.hpp											
+// Author : Pierre van Houtryve								
+////------------------------------------------------------//// 
+// This file contains the Type & TypeLoc classes
+////------------------------------------------------------////
+
+#pragma once
+
+#include "Fox/Common/Source.hpp"
+
+namespace fox
+{
+	class TypeBase;
+
+	// Type class, an observing pointer to a TypeBase*
+	// Used to facilitate passing TypeBase pointers as reference, (Type& instead of TypeBase*&)
+	// as well as adding flexibility in case I'd like to add Sugared types one day.
+	class Type
+	{
+		TypeBase* ty_ = nullptr;
+		public:
+			Type(TypeBase* ty = nullptr);
+
+			TypeBase* getPtr();
+			const TypeBase* getPtr() const;
+
+			bool isNull() const;
+
+			TypeBase* operator->();
+			const TypeBase* operator->() const;
+
+			explicit operator bool() const;
+	};
+
+	// A Type with it's SourceRange, which is used to represent "real" types written down
+	// by the user.
+	class TypeLoc : public Type
+	{
+		SourceRange range_;
+		public:
+			TypeLoc(TypeBase* ty = nullptr, SourceRange range = SourceRange());
+
+			SourceRange getRange() const;
+	};
+}
---
+++ lib/AST/Type.cpp
@@ -0,0 +1,66 @@
+#include "..\..\includes\Fox\AST\Type.hpp"
+#include "..\..\includes\Fox\AST\Type.hpp"
+#include "..\..\includes\Fox\AST\Type.hpp"
+////------------------------------------------------------////
+// This file is a part of The Moonshot Project.				
+// See LICENSE.txt for license info.						
+// File : Type.cpp											
+// Author : Pierre van Houtryve								
+////------------------------------------------------------//// 
+//			SEE HEADER FILE FOR MORE INFORMATION			
+////------------------------------------------------------////
+
+#include "Fox/AST/Type.hpp"
+
+using namespace fox;
+
+// Type
+
+Type::Type(TypeBase* ty) :
+	ty_(ty)
+{
+
+}
+
+TypeBase* Type::getPtr()
+{
+	return ty_;
+}
+
+const TypeBase* Type::getPtr() const
+{
+	return ty_;
+}
+
+bool Type::isNull() const
+{
+	return (ty_ == nullptr);
+}
+
+TypeBase* Type::operator->()
+{
+	return ty_;
+}
+
+const TypeBase* Type::operator->() const
+{
+	return ty_;
+}
+
+Type::operator bool() const
+{
+	return !isNull();
+}
+
+// TypeLoc
+
+TypeLoc::TypeLoc(TypeBase* ty, SourceRange range) :
+	Type(ty), range_(range)
+{
+
+}
+
+SourceRange TypeLoc::getRange() const
+{
+	return range_;
+}
