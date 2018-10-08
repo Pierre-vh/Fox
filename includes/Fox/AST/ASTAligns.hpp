2220fdca2fad98d852ffbd8ede00f09746975571
Pierre van Houtryve <pierre.vanhoutryve@outlook.com>
2018-10-08 16:07:33 +0200

    Adding ASTAligns.hpp

---
+++ includes/Fox/AST/ASTAligns.hpp
@@ -0,0 +1,17 @@
+////------------------------------------------------------////
+// This file is a part of The Moonshot Project.				
+// See LICENSE.txt for license info.						
+// File : ASTAligns.hpp											
+// Author : Pierre van Houtryve								
+////------------------------------------------------------//// 
+// This file contains various constants to be used as the minimum
+// alignement for some AST types.
+////------------------------------------------------------////
+
+#pragma once
+
+namespace fox {
+	namespace align {
+		constexpr unsigned TypeBaseAlignement = 2;
+	}
+}
