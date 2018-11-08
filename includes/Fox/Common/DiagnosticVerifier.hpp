240a827df50708c42e73add9e523185f30a024d5
Pierre van Houtryve <pierre.vanhoutryve@outlook.com>
2018-11-08 21:30:01 +0100

    WIP DiagnosticVerifier

--- includes/Fox/Common/DiagnosticVerifier.hpp
+++ includes/Fox/Common/DiagnosticVerifier.hpp
@@ -19,12 +19,13 @@
 
 namespace fox {
   class Diagnostic;
+  class DiagnosticEngine;
   class DiagnosticVerifier : public DiagnosticConsumer{
     using LineTy = CompleteLoc::LineTy;
     public:
       // Creates a DiagnosticVerifier. A DV will always require a consumer
       // attached to it, the value cannot be nullptr.
-      DiagnosticVerifier(SourceManager& srcMgr);
+      DiagnosticVerifier(DiagnosticEngine& engine, SourceManager& srcMgr);
 
       // Parses a file, searching for "expect-" directives, parsing them and 
       // adding them to the list of expected diagnostics.
@@ -37,6 +38,13 @@ namespace fox {
     private:
       void addExpectedDiag(FileID file, LineTy line, string_view str);
 
+      // Parses a single verify instr.
+      // The first argument is the loc of the first char of the instr.
+      bool handleVerifyInstr(SourceLoc loc, string_view instr);
+
+      void diagnoseMissingSuffix(SourceLoc instrBeg);
+
+      DiagnosticEngine& diags_;
       SourceManager& srcMgr_;
       // Map of expected diagnostics
       std::multimap<string_view, std::pair<FileID, LineTy>> expectedDiags_;
--- lib/Common/DiagnosticVerifier.cpp
+++ lib/Common/DiagnosticVerifier.cpp
@@ -6,6 +6,7 @@
 //----------------------------------------------------------------------------//
 
 #include "Fox/Common/DiagnosticVerifier.hpp"
+#include "Fox/Common/DiagnosticEngine.hpp"
 #include "Fox/Common/Diagnostic.hpp"
 
 using namespace fox;
@@ -15,22 +16,73 @@ using namespace fox;
 //----------------------------------------------------------------------------//
 
 namespace {
+  // Prefix used for verification
+  constexpr char vPrefix[] = "expect-";
+  constexpr std::size_t vPrefixSize = sizeof(vPrefix)-1;
 
+  // Offsets a sourceloc by X chars. This doesn't check if the SourceLoc
+  // is valid, it justs adds the offset to the index.
+  SourceLoc offsetSourceLoc(SourceLoc loc, std::size_t off) {
+    return SourceLoc(loc.getFileID(), loc.getIndex() + off);
+  }
+
+  // Removes characters such as \r, \n from the end of the string.
+  void removeNewline(string_view& str) {
+    std::size_t end = str.size();
+    for (auto it = str.rbegin(); it != str.rend(); ++it) {
+      char c = (*it);
+      if ((c == '\r') || (c == '\n'))
+        --end;
+      else break;
+    }
+
+    // See if trimming is necessary, if it is, do it.
+    if(end != str.size())
+      str = str.substr(0, end);
+  }
+
+  // Returns the whole string between pos and the end of the line/eof
+  string_view getRestOfLine(std::size_t pos, string_view str) {
+    auto end = str.find('\n', pos);
+    string_view rtr;
+    // If we found a \n, all good.
+    if(end != string_view::npos)
+      rtr = str.substr(pos, end-pos);
+    // If we didn't, take everything until the end of the file.
+    else
+      rtr = str.substr(pos, str.size()-pos);
+
+    removeNewline(rtr);
+    return rtr;
+  }
 } // anonymous namespace
 
 //----------------------------------------------------------------------------//
 //  DiagnosticVerifier's methods implementation
 //----------------------------------------------------------------------------//
 
-DiagnosticVerifier::DiagnosticVerifier(SourceManager& srcMgr):
-  srcMgr_(srcMgr) {
+DiagnosticVerifier::DiagnosticVerifier(DiagnosticEngine& engine, 
+  SourceManager& srcMgr): diags_(engine), srcMgr_(srcMgr) {
   
 }
 
 bool DiagnosticVerifier::parseFile(FileID fid) {
   // Fetch the content of the file
   string_view fStr = srcMgr_.getSourceStr(fid);
-
+  bool rtr = true;
+  {
+    std::size_t last = 0, idx = 0;
+    do {
+      idx = fStr.find(vPrefix, last);
+      if (idx == string_view::npos)
+        break;
+      last = idx + 1;
+      auto instr = getRestOfLine(idx, fStr);
+      std::cout << "Full instr found(" << instr << ")\n";
+      rtr |= handleVerifyInstr(SourceLoc(fid, idx), instr);
+    } while (true);
+  }
+  return false;
 }
 
 void DiagnosticVerifier::consume(Diagnostic& diag) {
@@ -62,4 +114,19 @@ void DiagnosticVerifier::consume(Diagnostic& diag) {
 void DiagnosticVerifier::addExpectedDiag(FileID file, LineTy line, 
   string_view str) {
   expectedDiags_.insert({str, {file, line}});
+}
+
+bool DiagnosticVerifier::handleVerifyInstr(SourceLoc loc, string_view instr) {
+  // Remove the prefix
+  instr = instr.substr(vPrefixSize, instr.size() - vPrefixSize);
+  diagnoseMissingSuffix(loc);
+  
+  return true;
+}
+
+void DiagnosticVerifier::diagnoseMissingSuffix(SourceLoc instrBeg) {
+  diags_.report(DiagID::diagverif_expectedsuffix,
+    offsetSourceLoc(instrBeg, vPrefixSize))
+      .addArg(vPrefix)
+      .setExtraRange(SourceRange(instrBeg, vPrefixSize - 1));
 }
