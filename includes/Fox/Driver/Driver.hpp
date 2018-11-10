392fdba4f2170893e26b84f8f3462e6d96f1c24c
Pierre van Houtryve <pierre.vanhoutryve@outlook.com>
2018-11-10 14:35:04 +0100

    [Doc] Update

--- includes/Fox/Driver/Driver.hpp
+++ includes/Fox/Driver/Driver.hpp
@@ -8,9 +8,6 @@
 // 
 // At first, this driver will just take a file path as input and try
 // to compile it, emitting informations to a user-defined ostream.
-// (By try to compile it, I mean, with what's available. At the time of writing theses
-// lines, the driver attempts to parse a single function definition and It'll output
-// the AST and other Diag informations.)
 //----------------------------------------------------------------------------//
 
 #include <string>
@@ -20,7 +17,6 @@ namespace fox {
   class Driver {
     public:  
       Driver() = default;
-      // This function will lex a file and call parseFuncDecl, then output the ast.
       bool processFile(std::ostream& out, const std::string& filepath);
   };
 }
