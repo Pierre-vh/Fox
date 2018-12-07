bc1b6e0b074c197087fa0359618d856417e118de
Pierre-vh <pierre.vanhoutryve@outlook.com>
2018-12-07 08:47:15 +0100

    Adding SemaStmt

---
+++ lib/Sema/SemaStmt.cpp
@@ -0,0 +1,12 @@
+//----------------------------------------------------------------------------//
+// This file is a part of The Moonshot Project.        
+// See LICENSE.txt for license info.            
+// File : SemaStmt.cpp                    
+// Author : Pierre van Houtryve                
+//----------------------------------------------------------------------------//
+//  This file implements semantic analysis for statements.
+//----------------------------------------------------------------------------//
+
+#include "Fox/Sema/Sema.hpp"
+
+using namespace fox;
