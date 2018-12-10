d48bf66fb3617d50b2ce7025febbc2732ede54bb
Pierre-vh <pierre.vanhoutryve@outlook.com>
2018-12-10 09:56:13 +0100

    Adding Scope class

---
+++ includes/Fox/Parser/Scope.hpp
@@ -0,0 +1,52 @@
+//----------------------------------------------------------------------------//
+// This file is a part of The Moonshot Project.        
+// See the LICENSE.txt file at the root of the project for license information.            
+// File : Scope.hpp                      
+// Author : Pierre van Houtryve                
+//----------------------------------------------------------------------------//
+// This file implements the Scope class, which is used to perform
+// local variable binding in the Parser.
+//----------------------------------------------------------------------------//
+
+#pragma once
+
+#include <map>
+#include <vector>
+#include "Fox/AST/Identifier.hpp"
+
+namespace fox {
+  class NamedDecl;
+
+  // This class represents a single scope, and contains pointer a pointer to
+  // the parent scope. This is pretty similar to a DeclContext, but simplified.
+  class Scope {
+    public:
+      using LookupResultTy = std::vector<NamedDecl*>;
+      
+      // Adds a declaration so it's visible in this current scope
+      // If a declaration with this name already exists in this scope, or it's
+      // parent, the decl won't be added and we'll return the a pointer to 
+      // the first (in lexical order) occurence of a decl with the same name. 
+      NamedDecl* add(NamedDecl* decl);
+
+      // Performs a lookup in this scope, populating the "result&"
+      // vector with every found decl.
+      //
+      // The result vector is populated in a reverse order. The first result
+      // is the latest occurence, the last result is the earliest one.
+      void lookup(Identifier id, LookupResultTy& results);
+
+      Scope* getParent() const;
+      bool hasParent() const;
+      void setParent(Scope* scope);
+
+    private:
+      // Searches only in this scope instance's map.
+      void lookupImpl(Identifier id, LookupResultTy& results);
+
+      // The parent scope
+      Scope* parent_ = nullptr;
+      // The decls present in this scope
+      std::map<Identifier, NamedDecl*> decls_;
+  };
+}
---
+++ lib/Parser/Scope.cpp
@@ -0,0 +1,55 @@
+//----------------------------------------------------------------------------//
+// This file is a part of The Moonshot Project.        
+// See LICENSE.txt for license info.            
+// File : Scope.cpp                    
+// Author : Pierre van Houtryve                
+//----------------------------------------------------------------------------//
+
+#include "Fox/Parser/Scope.hpp"
+#include "Fox/AST/Decl.hpp"
+
+using namespace fox;
+
+NamedDecl* Scope::add(NamedDecl* decl) {
+  LookupResultTy result;
+  Identifier id = decl->getIdentifier();
+  lookup(id, result);
+  // Decl already exists in this Scope.
+  if(result.size()) {
+    // In theory, only one result should have been found since
+    // we never add duplicates
+    assert(result.size() == 1);
+    return result.back();
+  }
+  // Decl doesn't exist, add it in this scope.
+  auto res = decls_.insert({ id, decl });
+  assert(res.second && "No insertion occured?");
+  // Success
+  return nullptr;
+}
+
+void Scope::lookup(Identifier id, LookupResultTy& results) {
+  Scope* cur = this;
+  while (cur) {
+    cur->lookupImpl(id, results);
+    cur = cur->getParent();
+  }
+}
+
+void Scope::lookupImpl(Identifier id, LookupResultTy& results) {
+  auto it = decls_.find(id);
+  if (it != decls_.end())
+    results.push_back(it->second);
+}
+
+Scope* Scope::getParent() const {
+  return parent_;
+}
+
+bool Scope::hasParent() const {
+  return (bool)parent_;
+}
+
+void Scope::setParent(Scope* scope) {
+  parent_ = scope;
+}
