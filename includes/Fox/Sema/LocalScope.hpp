617dc6afa848cfed075f37a580d75a4b6e4717ea
Pierre-vh <pierre.vanhoutryve@outlook.com>
2018-12-11 13:34:19 +0100

    Update LocalScope interface

--- includes/Fox/Sema/LocalScope.hpp
+++ includes/Fox/Sema/LocalScope.hpp
@@ -4,8 +4,7 @@
 // File : LocalScope.hpp                      
 // Author : Pierre van Houtryve                
 //----------------------------------------------------------------------------//
-// This file implements the LocalScope class, which is used to perform
-// local variable binding.
+// This file contains the LocalScope class.
 //----------------------------------------------------------------------------//
 
 #pragma once
@@ -19,37 +18,44 @@ namespace fox {
 
   // This class represents a single scope, and contains pointer a pointer to
   // the parent scope. This is pretty similar to a DeclContext, but simplified.
-  // One of theses is created by Sema when we enter a local DeclContext.
+  //
+  // Theses are created by Sema when it enters a local DeclContext.
+  //
+  // Note that this class is pretty trivial. It won't check if a decl
+  // was inserted twice or anything like that. 
   class LocalScope {
     public:
       using LookupResultTy = std::vector<NamedDecl*>;
+      using MapTy = std::multimap<Identifier, NamedDecl*>;
       
       LocalScope(LocalScope* parent = nullptr);
 
-      // Adds a declaration so it's visible in this current scope
-      // If a declaration with this name already exists in this scope, or it's
-      // parent, the decl won't be added and we'll return the a pointer to 
-      // the first (in lexical order) occurence of a decl with the same name. 
-      NamedDecl* add(NamedDecl* decl);
+      // Adds a declaration in this LocalScope.
+      // Note that "decl" must have a valid Identifier()
+      void add(NamedDecl* decl);
 
-      // Performs a lookup in this scope, populating the "result&"
-      // vector with every found decl.
+      // Performs a search in this scope, populating the "result&"
+      // vector with every found NamedDecl* that have "id" as Identifier.
       //
       // The result vector is populated in a reverse order. The first result
       // is the latest occurence, the last result is the earliest one.
-      void lookup(Identifier id, LookupResultTy& results);
+      void search(Identifier id, LookupResultTy& results);
+
+      // Returns the map of (Identifier -> NamedDecl*) used internally to
+      // store declarations in this scope.
+      MapTy& getMap();
 
       LocalScope* getParent() const;
       bool hasParent() const;
       void setParent(LocalScope* scope);
 
     private:
-      // Searches only in this scope instance's map.
-      void lookupImpl(Identifier id, LookupResultTy& results);
+      // Searches only in this scope's map.
+      void searchImpl(Identifier id, LookupResultTy& results);
 
       // The parent scope
       LocalScope* parent_ = nullptr;
       // The decls present in this scope
-      std::map<Identifier, NamedDecl*> decls_;
+      MapTy decls_;
   };
 }
--- lib/Sema/LocalScope.cpp
+++ lib/Sema/LocalScope.cpp
@@ -12,36 +12,29 @@ using namespace fox;
 
 LocalScope::LocalScope(LocalScope* parent) : parent_(parent) {}
 
-NamedDecl* LocalScope::add(NamedDecl* decl) {
-  LookupResultTy result;
+void LocalScope::add(NamedDecl* decl) {
   Identifier id = decl->getIdentifier();
-  lookup(id, result);
-  // Decl already exists in this LocalScope.
-  if(result.size()) {
-    // In theory, only one result should have been found since
-    // we never add duplicates
-    assert(result.size() == 1);
-    return result.back();
-  }
-  // Decl doesn't exist, add it in this scope.
-  auto res = decls_.insert({ id, decl });
-  assert(res.second && "No insertion occured?");
-  // Success
-  return nullptr;
+  assert(id && "decl must have a valid Identifier!");
+  decls_.insert({ id, decl });
 }
 
-void LocalScope::lookup(Identifier id, LookupResultTy& results) {
+void LocalScope::search(Identifier id, LookupResultTy& results) {
   LocalScope* cur = this;
+
   while (cur) {
-    cur->lookupImpl(id, results);
+    cur->searchImpl(id, results);
     cur = cur->getParent();
   }
 }
 
-void LocalScope::lookupImpl(Identifier id, LookupResultTy& results) {
+void LocalScope::searchImpl(Identifier id, LookupResultTy& results) {
   auto it = decls_.find(id);
   if (it != decls_.end())
     results.push_back(it->second);
+}
+
+LocalScope::MapTy& LocalScope::getMap() {
+  return decls_;
 }
 
 LocalScope* LocalScope::getParent() const {
