b29f1f4a966e688309e51d823185b4b3d63e78cf
Pierre van Houtryve <pierre.vanhoutryve@outlook.com>
2018-12-19 14:50:13 +0100

    Adding SmallVector to LLVM.hpp

--- includes/Fox/Common/LLVM.hpp
+++ includes/Fox/Common/LLVM.hpp
@@ -12,10 +12,19 @@
 // that can't be easily forward-declared. 
 #include "llvm/Support/Casting.h"
 
+// Forward-declare SmallVector 
+namespace llvm {
+  template <typename T> class SmallVectorImpl;
+  template <typename T, unsigned N> class SmallVector;
+}
+
 namespace fox {
   using llvm::isa;
   using llvm::cast;
   using llvm::dyn_cast;
   using llvm::dyn_cast_or_null;
   using llvm::cast_or_null;
+
+  using llvm::SmallVectorImpl;
+  using llvm::SmallVector;
 }
