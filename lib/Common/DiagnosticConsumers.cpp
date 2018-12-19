0403ae4d9a9e2c3269b6fe3ebafc4dcace7f68b4
Pierre van Houtryve <pierre.vanhoutryve@outlook.com>
2018-12-19 17:49:03 +0100

    Don't bother calculating the complete loc for file wide diagnostics

--- lib/Common/DiagnosticConsumers.cpp
+++ lib/Common/DiagnosticConsumers.cpp
@@ -29,11 +29,11 @@ std::string DiagnosticConsumer::getLocInfo(SourceManager& sm,
   const auto* sourceData = sm.getSourceData(file);
 
   std::stringstream ss;
-  ss << "<" << sourceData->fileName << '>';
+  ss << "<" << sourceData->fileName << ">:";
 
-  // If not fileWide, add the range as string.
-  if(!isFileWide)
-    ss << ":" << range.toString(sm);
+  // For FileWide diags, just use "1" as the line. For normal diags,
+  // use range.toString
+  ss << (isFileWide ? "1" : range.toString(sm));
   return ss.str();
 }
 
