0d6b98a6a640cee91388a3c39c11e71555bebd54
Pierre van Houtryve <pierre.vanhoutryve@outlook.com>
2019-01-09 17:44:30 +0100

    [Doc][Expr] Using doxygen-style doc

--- includes/Fox/AST/Expr.hpp
+++ includes/Fox/AST/Expr.hpp
@@ -24,8 +24,7 @@ namespace fox   {
   class ASTContext;
   class TypeBase;
 
-  // This enum represents every possible Expression subclass.
-  // It is automatically generated using Fox/AST/ExprNodes.def
+  /// Enum representing every kind of expression that exist.
   enum class ExprKind: std::uint8_t {
     #define EXPR(ID,PARENT) ID,
     #define EXPR_RANGE(ID, FIRST, LAST) First_##ID = FIRST, Last_##ID = LAST,
@@ -35,8 +34,8 @@ namespace fox   {
   // ostream to print kinds
   std::ostream& operator<<(std::ostream& os, ExprKind kind);
 
-  // Expr
-  //    Common base class for every expression
+  /// Expr
+  ///    Common base class for every expression
   class alignas(ExprAlignement) Expr {
     public:
       ExprKind getKind() const;
@@ -48,7 +47,7 @@ namespace fox   {
       void setType(Type type);
       Type getType() const;
 
-      // Debug method. Does a ASTDump of this node to std::cerr
+      /// Debug method. Does a ASTDump of this node to std::cerr
       void dump() const;
 
     protected:
@@ -71,11 +70,11 @@ namespace fox   {
       Type type_;
   };
 
-  // A Vector of Expr*
+  /// A Vector of Expr*
   using ExprVector = SmallVector<Expr*, 4>;
 
-  // BinaryExpr
-  //    A binary expression
+  /// BinaryExpr
+  ///    A binary expression
   class BinaryExpr final : public Expr {
     public:
       enum class OpKind: std::uint8_t {
@@ -95,7 +94,7 @@ namespace fox   {
       void setOp(OpKind op);
       OpKind getOp() const;
 
-      // Checks that the operation isn't OpKind::Invalid
+      /// Checks that the operation isn't OpKind::Invalid
       bool isValidOp() const;
       // + (only concat)
       bool isConcat() const;
@@ -121,16 +120,16 @@ namespace fox   {
         return (expr->getKind() == ExprKind::BinaryExpr);
       }
 
-      // Returns the user-readable sign of this expression's 
-      // operator as a string
+      /// Returns the user-readable sign of this expression's 
+      /// operator as a string
       std::string getOpSign() const;
 
-      // Returns the user-readable name of this expression's
-      // operator as a string.
+      /// Returns the user-readable name of this expression's
+      /// operator as a string.
       std::string getOpName() const;
 
-      // Returns the user-readble Id (kind) of this expression's
-      // operator as a string.
+      /// Returns the user-readble Id (kind) of this expression's
+      /// operator as a string.
       std::string getOpID() const;
 
     private:
@@ -142,8 +141,8 @@ namespace fox   {
       OpKind op_ = OpKind::Invalid;
   };
 
-  // UnaryExpr
-  //    A prefix unary expression: -2
+  /// UnaryExpr
+  ///    A prefix unary expression: -2
   class UnaryExpr final : public Expr {
     public: 
       enum class OpKind: std::uint8_t {
@@ -167,13 +166,13 @@ namespace fox   {
         return (expr->getKind() == ExprKind::UnaryExpr);
       }
 
-      // Returns the user-readable sign of the operator as a string
+      /// Returns the user-readable sign of the operator as a string
       std::string getOpSign() const;
 
-      // Returns the user-readable name as a string
+      /// Returns the user-readable name as a string
       std::string getOpName() const;
 
-      // Returns the user-readble Id (kind) as a string
+      /// Returns the user-readble Id (kind) as a string
       std::string getOpID() const;
 
     private:
@@ -184,8 +183,8 @@ namespace fox   {
       OpKind op_ = OpKind::Invalid;
   };
 
-  // CastExpr
-  //    An explicit "as" cast expression: foo as int
+  /// CastExpr
+  ///    An explicit "as" cast expression: foo as int
   class CastExpr final : public Expr {
     public:      
       static CastExpr* create(ASTContext& ctxt, TypeLoc castGoal, Expr* expr);
@@ -200,8 +199,8 @@ namespace fox   {
 
       SourceRange getRange() const;
 
-      // Returns true if this cast is considered "useless"
-      //  e.g. "0 as int" is a useless cast.
+      /// Returns true if this cast is considered "useless"
+      ///  e.g. "0 as int" is a useless cast.
       bool isUseless() const;
       void markAsUselesss();
 
@@ -219,8 +218,8 @@ namespace fox   {
       llvm::PointerIntPair<Expr*, 1, bool> exprAndIsUseless_;
   };
 
-  // AnyLiteralExpr
-  //  Common base class for any literal expression.
+  /// AnyLiteralExpr
+  ///  Common base class for any literal expression.
   class AnyLiteralExpr : public Expr {
     public:
       SourceRange getRange() const;
@@ -239,8 +238,8 @@ namespace fox   {
       SourceRange range_;
   };
 
-  // CharLiteralExpr
-  //    A char literal: 'a'
+  /// CharLiteralExpr
+  ///    A char literal: 'a'
   class CharLiteralExpr : public AnyLiteralExpr {
     public:
       static CharLiteralExpr* create(ASTContext& ctxt,
@@ -258,8 +257,8 @@ namespace fox   {
       const FoxChar val_ = ' ';
   };
 
-  // IntegerLiteralExpr
-  //    An integer literal: 2
+  /// IntegerLiteralExpr
+  ///    An integer literal: 2
   class IntegerLiteralExpr final : public AnyLiteralExpr {
     public:
       static IntegerLiteralExpr* create(ASTContext& ctxt, 
@@ -277,8 +276,8 @@ namespace fox   {
       const FoxInt val_ = 0;
   };
 
-  // DoubleLiteralExpr
-  //    A double literal: 3.14
+  /// DoubleLiteralExpr
+  ///    A double literal: 3.14
   class DoubleLiteralExpr final : public AnyLiteralExpr {
     public:
       static DoubleLiteralExpr* create(ASTContext& ctxt, FoxDouble val,
@@ -296,8 +295,8 @@ namespace fox   {
       const FoxDouble val_ = 0.0;
   };
 
-  // StringLiteralExpr
-  //    A string literal: "foo"
+  /// StringLiteralExpr
+  ///    A string literal: "foo"
   class StringLiteralExpr final : public AnyLiteralExpr {
     public:
       static StringLiteralExpr* create(ASTContext& ctxt, string_view val,
@@ -315,8 +314,8 @@ namespace fox   {
       const string_view val_ = "";
   };
 
-  // BoolLiteralExpr
-  //    true/false boolean literal
+  /// BoolLiteralExpr
+  ///    true/false boolean literal
   class BoolLiteralExpr final : public AnyLiteralExpr {
     public:
       static BoolLiteralExpr* create(ASTContext& ctxt, bool val, 
@@ -334,8 +333,8 @@ namespace fox   {
       const bool val_ : 1;
   };
 
-  // ArrayLiteralExpr
-  //    An array literal: [1, 2, 3]
+  /// ArrayLiteralExpr
+  ///    An array literal: [1, 2, 3]
   class ArrayLiteralExpr final : public AnyLiteralExpr, 
     llvm::TrailingObjects<ArrayLiteralExpr, Expr*> {
     friend TrailingObjects;
@@ -363,8 +362,8 @@ namespace fox   {
       const SizeTy numElems_;
   };
 
-  // UnresolvedExpr
-  //    A small common base class for unresolved expressions
+  /// UnresolvedExpr
+  ///    A small common base class for unresolved expressions
   class UnresolvedExpr : public Expr {
     public:
       static bool classof(const Expr* expr) {
@@ -378,8 +377,8 @@ namespace fox   {
       UnresolvedExpr(ExprKind kind);
   };
 
-  // UnresolvedDeclRefExpr
-  //    Represents a unresolved DeclRef
+  /// UnresolvedDeclRefExpr
+  ///    Represents a unresolved DeclRef
   class UnresolvedDeclRefExpr final : public UnresolvedExpr {
     public:
       static UnresolvedDeclRefExpr* create(ASTContext& ctxt, Identifier id,
@@ -400,8 +399,8 @@ namespace fox   {
       Identifier id_;
   };
 
-  // DeclRefExpr
-  //    A resolved reference to a ValueDecl. 
+  /// DeclRefExpr
+  ///    A resolved reference to a ValueDecl. 
   class DeclRefExpr final : public Expr {
     public:
       static DeclRefExpr* create(ASTContext& ctxt, ValueDecl* decl,
@@ -423,8 +422,8 @@ namespace fox   {
       ValueDecl* decl_ = nullptr;
   };
 
-  // MemberOfExpr
-  //    A member access : foo.bar
+  /// MemberOfExpr
+  ///    A member access : foo.bar
   class MemberOfExpr final : public Expr {
     public:
       static MemberOfExpr* create(ASTContext& ctxt, Expr* base, 
@@ -454,8 +453,8 @@ namespace fox   {
       Identifier memb_;
   };
 
-  // ArraySubscriptExpr
-  //    Array access (or subscript): foo[3]
+  /// ArraySubscriptExpr
+  ///    Array access (or subscript): foo[3]
   class ArraySubscriptExpr final : public Expr {
     public:
       static ArraySubscriptExpr* create(ASTContext& ctxt, Expr* base, 
@@ -481,8 +480,8 @@ namespace fox   {
       Expr* idxExpr_ = nullptr;
   };
 
-  // CallExpr
-  //    A function call: foo(3.14)
+  /// CallExpr
+  ///    A function call: foo(3.14)
   class CallExpr final : public Expr, 
     llvm::TrailingObjects<CallExpr, Expr*> {
     friend class TrailingObjects;
@@ -502,8 +501,8 @@ namespace fox   {
       Expr* getArg(std::size_t idx) const;
       void setArg(Expr* arg, std::size_t idx);
 
-      // Returns a SourceRange that covers every argument passed
-      // to the Call. Returns an invalid SourceRange if numArgs() == 0.
+      /// Returns a SourceRange that covers every argument passed
+      /// to the Call. Returns an invalid SourceRange if numArgs() == 0.
       SourceRange getArgsRange() const;
 
       SourceRange getRange() const;
