9581df25a557bc496dfd6e31bc61ea11d0db24f5
Pierre van Houtryve <pierre.vanhoutryve@outlook.com>
2019-02-11 18:05:11 +0100

    Add leak checking in tests

--- unittests/src/main.cpp
+++ unittests/src/main.cpp
@@ -8,11 +8,22 @@
 //----------------------------------------------------------------------------//
 
 #include "gtest/gtest.h"
-#include <iostream>
+
+// Check if we can leak-check using _Crt leak-checking tools
+#if defined(_MSC_VER) && !defined(_NDEBUG)
+  #define CAN_LEAK_CHECK_ON_MSVC 1
+  #define _CRTDBG_MAP_ALLOC  
+  #include <stdlib.h>  
+  #include <crtdbg.h>  
+#else
+  #define CAN_LEAK_CHECK_ON_MSVC 0
+#endif 
 
 int main(int argc, char **argv) {
+  // Enable leak checking under MSVC.
+  #if CAN_LEAK_CHECK_ON_MSVC
+    _CrtSetDbgFlag ( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
+  #endif
   ::testing::InitGoogleTest(&argc, argv);
-  auto res = RUN_ALL_TESTS();
-  std::cin.get();
-  return res;
+  return RUN_ALL_TESTS();
 }
