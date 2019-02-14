7400196cad27ebd4b98990979dbc075f8a79b434
Pierre-vh <pierre.vanhoutryve@outlook.com>
2019-02-14 14:02:42 +0100

    Reordering method to match declaration order in header

--- lib/AST/DeclContext.cpp
+++ lib/AST/DeclContext.cpp
@@ -14,21 +14,61 @@
 using namespace fox;
 
 //----------------------------------------------------------------------------//
-// DeclContext
+// DeclIterator
 //----------------------------------------------------------------------------//
 
-DeclContext::DeclContext(ASTContext& ctxt, DeclContextKind kind, 
-                         DeclContext* parent):
-  parentAndKind_(parent, toInt(kind)) {
-  assert((parent || isa<UnitDecl>(this)) && "Every DeclContexts except "
-    "UnitDecls must have a parent!");
-  // Create the LookupMap
-  void* mem = ctxt.allocate(sizeof(LookupMap), alignof(LookupMap));
-  lookupMap_ = new(mem) LookupMap();
-  // Add its cleanup
-  ctxt.addDestructorCleanup(*lookupMap_);
-  assert(lookupMap_);
+DeclIterator::DeclIterator(Decl* cur) : cur_(cur) {}
+
+DeclIterator& DeclIterator::operator++() {
+  assert(cur_ && "incrementing past the end");
+  cur_ = cur_->nextDecl_;
+  return *this;
 }
+
+DeclIterator fox::DeclIterator::operator++(int) {
+  DeclIterator old = *this;
+  ++(*this);
+  return old;
+}
+
+Decl* DeclIterator::operator*() const {
+  return cur_;
+}
+
+Decl* DeclIterator::operator->() const {
+  return cur_;
+}
+
+bool fox::operator==(DeclIterator lhs, DeclIterator rhs) {
+  return lhs.cur_ == rhs.cur_;
+}
+
+bool fox::operator!=(DeclIterator lhs, DeclIterator rhs) {
+  return lhs.cur_ != rhs.cur_;
+}
+
+//----------------------------------------------------------------------------//
+// DeclRange
+//----------------------------------------------------------------------------//
+
+DeclRange::DeclRange(DeclIterator beg, DeclIterator end) 
+  : beg_(beg), end_(end) {}
+
+DeclIterator DeclRange::begin() const {
+  return beg_;
+}
+
+DeclIterator DeclRange::end() const {
+  return end_;
+}
+
+bool DeclRange::isEmpty() const {
+  return beg_ == end_;
+}
+
+//----------------------------------------------------------------------------//
+// DeclContext
+//----------------------------------------------------------------------------//
 
 DeclContextKind DeclContext::getDeclContextKind() const {
   return static_cast<DeclContextKind>(parentAndKind_.getInt());
@@ -49,8 +89,7 @@ DeclContext* DeclContext::getParentDeclCtxt() const {
 }
 
 bool DeclContext::isLocal() const {
-  return getDeclContextKind() == DeclContextKind::FuncDecl
-  ;
+  return getDeclContextKind() == DeclContextKind::FuncDecl;
 }
 
 void DeclContext::addDecl(Decl* decl) {
@@ -120,55 +159,15 @@ bool DeclContext::classof(const Decl* decl) {
   }
 }
 
-//----------------------------------------------------------------------------//
-// DeclIterator
-//----------------------------------------------------------------------------//
-
-DeclIterator::DeclIterator(Decl* cur) : cur_(cur) {}
-
-DeclIterator& DeclIterator::operator++() {
-  assert(cur_ && "incrementing past the end");
-  cur_ = cur_->nextDecl_;
-  return *this;
-}
-
-DeclIterator fox::DeclIterator::operator++(int) {
-  DeclIterator old = *this;
-  ++(*this);
-  return old;
-}
-
-Decl* DeclIterator::operator*() const {
-  return cur_;
-}
-
-Decl* DeclIterator::operator->() const {
-  return cur_;
-}
-
-bool fox::operator==(DeclIterator lhs, DeclIterator rhs) {
-  return lhs.cur_ == rhs.cur_;
-}
-
-bool fox::operator!=(DeclIterator lhs, DeclIterator rhs) {
-  return lhs.cur_ != rhs.cur_;
-}
-
-//----------------------------------------------------------------------------//
-// DeclRange
-//----------------------------------------------------------------------------//
-
-DeclRange::DeclRange(DeclIterator beg, DeclIterator end) 
-  : beg_(beg), end_(end) {}
-
-DeclIterator DeclRange::begin() const {
-  return beg_;
-}
-
-DeclIterator DeclRange::end() const {
-  return end_;
-}
-
-bool DeclRange::isEmpty() const {
-  return beg_ == end_;
+DeclContext::DeclContext(ASTContext& ctxt, DeclContextKind kind, 
+                         DeclContext* parent):
+  parentAndKind_(parent, toInt(kind)) {
+  assert((parent || isa<UnitDecl>(this)) && "Every DeclContexts except "
+    "UnitDecls must have a parent!");
+  // Create the LookupMap
+  void* mem = ctxt.allocate(sizeof(LookupMap), alignof(LookupMap));
+  lookupMap_ = new(mem) LookupMap();
+  // Add its cleanup
+  ctxt.addDestructorCleanup(*lookupMap_);
+  assert(lookupMap_);
 }
