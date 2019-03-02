93e6cde1ff2b9a78f2463ad37f4ca1b7c983021b
Pierre van Houtryve <pierre.vanhoutryve@outlook.com>
2019-03-02 16:35:31 +0100

    Initial VM scaffolding

--- includes/Fox/VM/VM.hpp
+++ includes/Fox/VM/VM.hpp
@@ -9,6 +9,34 @@
 
 #pragma once
 
-namespace fox {
+#include <cstdint>
+#include <cstddef>
+#include <array>
+#include "llvm/ADT/ArrayRef.h"
+#include "Fox/Common/LLVM.hpp"
 
+namespace fox {
+  class VM {
+    public:
+      static constexpr unsigned numStackRegister = 255;
+
+      // Loads a program
+      void load(ArrayRef<std::uint32_t> instrs);
+
+      // Runs the current program.
+      void run();
+
+      ArrayRef<std::uint64_t> getRegisterStack() const;
+      MutableArrayRef<std::uint64_t> getRegisterStack();
+
+    private:
+      // The program currently being executed
+      ArrayRef<std::uint32_t> program_;
+
+      // The program counter
+      std::uint64_t programCounter_ = 0;
+
+      // The registers
+      std::array<std::uint64_t, numStackRegister> regStack_;
+  };
 }
--- lib/VM/VM.cpp
+++ lib/VM/VM.cpp
@@ -8,3 +8,20 @@
 #include "Fox/VM/VM.hpp"
 
 using namespace fox;
+
+void VM::load(ArrayRef<std::uint32_t> instrs) {
+  program_ = instrs;
+  programCounter_ = 0;
+}
+
+void VM::run() {
+  // TODO
+}
+
+ArrayRef<std::uint64_t> VM::getRegisterStack() const {
+  return regStack_;
+}
+
+MutableArrayRef<std::uint64_t> VM::getRegisterStack() {
+  return regStack_;
+}
