7a803fbb87f4a79b36ba1988767e8c4f2ebabb4c
Pierre-vh <pierre.vanhoutryve@outlook.com>
2019-03-14 09:49:04 +0100

    WIP BCGenExpr
    -> Generation of non-assignement BinaryExpr on integers should work now. Still needs to be tested!

--- lib/BCGen/BCGenExpr.cpp
+++ lib/BCGen/BCGenExpr.cpp
@@ -9,6 +9,7 @@
 #include "Fox/BCGen/BCGen.hpp"
 #include "Fox/VM/InstructionBuilder.hpp"
 #include "Fox/AST/Expr.hpp"
+#include "Fox/AST/Types.hpp"
 #include "Fox/AST/ASTVisitor.hpp"
 #include "Fox/AST/ASTWalker.hpp"
 #include "Fox/Common/FoxTypes.hpp"
@@ -17,7 +18,7 @@ using namespace fox;
 
 //----------------------------------------------------------------------------//
 // ExprGenerator
-//----------------------------------------------------------------------------//
+//----------------------------------------------------------------------------// 
 
 // The actual class responsible for generating the bytecode of expressions
 class BCGen::ExprGenerator : public Generator,
@@ -37,6 +38,8 @@ class BCGen::ExprGenerator : public Generator,
     RegisterAllocator& regAlloc;
 
   private:
+    using BinOp = BinaryExpr::OpKind;
+
     //------------------------------------------------------------------------//
     // "emit" methods 
     // 
@@ -52,12 +55,120 @@ class BCGen::ExprGenerator : public Generator,
                      int16_max = std::numeric_limits<std::int16_t>::max();
       // Check if the value fits in a int16. In that case, emit a StoreSmallInt
       if ((val >= int16_min) && (val <= int16_max)) {
-        builder.createStoreSmallIntInstr(dest.getRegisterNumber(), val);
+        builder.createStoreSmallIntInstr(dest.getAddress(), val);
       }
       // Else, for now, do nothing because I need the constant table to 
       // emit constants large than that.
       fox_unimplemented_feature("Emission & Storage of constants larger than "
         "16 bits");
+    }
+
+    // Generates the adequate instruction to perform a given binary
+    // operation on 'lhs' and 'rhs' (registers containing doubles), putting
+    // the result in 'rhs'
+    /*
+    void genBinaryOperationOnDoubles(BinOp op, const RegisterValue& dest, 
+      RegisterValue lhs, RegisterValue rhs) {
+      // TODO
+    }
+    */
+
+    // Generates the adequate instruction to perform a given binary
+    // operation on 'lhs' and 'rhs' (registers containing ints), putting
+    // the result in 'rhs'
+    void genBinaryOperationOnInts(BinOp op, const RegisterValue& dest, 
+      RegisterValue lhs, RegisterValue rhs) {
+      // Fetch the addresses
+      regnum_t destAddr = dest.getAddress();
+      regnum_t lhsAddr = lhs.getAddress();
+      regnum_t rhsAddr = rhs.getAddress();
+      // Emit
+      switch (op) {
+        case BinOp::Add:  // +
+          builder.createAddIntInstr(destAddr, lhsAddr, rhsAddr);
+          break;
+        case BinOp::Sub:  // -
+          builder.createSubIntInstr(destAddr, lhsAddr, rhsAddr);
+          break;
+        case BinOp::Mul:  // *
+          builder.createMulIntInstr(destAddr, lhsAddr, rhsAddr);
+          break;
+        case BinOp::Div:  // /
+          builder.createDivIntInstr(destAddr, lhsAddr, rhsAddr);
+          break;
+        case BinOp::Mod:  // %
+          builder.createModIntInstr(destAddr, lhsAddr, rhsAddr);
+          break;
+        case BinOp::Pow:  // **
+          builder.createPowIntInstr(destAddr, lhsAddr, rhsAddr);
+          break;
+        case BinOp::LE:   // <=
+          builder.createLEIntInstr(destAddr, lhsAddr, rhsAddr);
+          break;
+        case BinOp::GE:   // >=
+          // For >=, it's not implemented in the VM, but
+          // (a >= b) is the same as (b <= a)
+          builder.createLEIntInstr(destAddr, rhsAddr, lhsAddr);
+        case BinOp::LT:   // <
+          builder.createLTIntInstr(destAddr, lhsAddr, rhsAddr);
+          break;
+        case BinOp::GT:   // >
+          // > isn't implemented in the VM too, but
+          // (a > b) is the same as !(a <= b). This requires 2 instructions.
+          // dest = lhs <= rhs
+          builder.createLEIntInstr(destAddr, lhsAddr, rhsAddr);
+          // dest != dest
+          builder.createLNotInstr(destAddr, destAddr);
+        case BinOp::Eq:   // ==
+          builder.createEqIntInstr(destAddr, lhsAddr, rhsAddr);
+        case BinOp::NEq:  // !=
+          // != isn't implemented in the vm, it's just implemented
+          // as !(a == b). This requires 2 instructions.
+          builder.createEqIntInstr(destAddr, lhsAddr, rhsAddr);
+          builder.createLNotInstr(destAddr, destAddr);
+        default:
+          fox_unreachable("Unhandled binary operation kind");
+      }
+    }
+
+    RegisterValue genNumericBinaryExpr(BinaryExpr* expr) {
+      assert((expr->getType()->isNumeric()) && "expr is not numeric");
+      
+      // Gen the LHS
+      RegisterValue lhsReg;
+      {
+        Expr* lhsExpr = expr->getLHS();
+        assert(lhsExpr->getType()->isNumeric() 
+          && "BinaryExpr is numeric but the LHS isn't!");
+        lhsReg = visit(lhsExpr);
+        assert(lhsReg.isAlive() && "Generated a dead register for the LHS");
+      }
+
+      // Gen the RHS
+      RegisterValue rhsReg;
+      {
+        Expr* rhsExpr = expr->getRHS();
+        assert(rhsExpr->getType()->isNumeric() 
+          && "BinaryExpr is numeric but the RHS isn't!");
+        rhsReg = visit(rhsExpr);
+        assert(rhsReg.isAlive() && "Generated a dead register for the RHS");
+      }
+
+      // Gen the Op
+      RegisterValue destReg = regAlloc.allocateNewRegister();
+      if (expr->getType()->isIntType()) {
+        genBinaryOperationOnInts(expr->getOp(), destReg, 
+                                 std::move(lhsReg), std::move(rhsReg));
+      } 
+      else if (expr->getType()->isDoubleType()) {
+        // TODO
+        fox_unimplemented_feature("Floating-point BinaryExpr BCGen");
+        /*genBinaryOperationOnDoubles(expr->getOp(), destReg, 
+                                 std::move(lhsReg), std::move(rhsReg));*/
+      }
+      else 
+        fox_unreachable("Unknown Numeric Type Kind");
+      return destReg;
     }
 
     //------------------------------------------------------------------------//
@@ -67,34 +178,33 @@ class BCGen::ExprGenerator : public Generator,
     // the bytecode for an Expr.
     //------------------------------------------------------------------------//
 
-    RegisterValue visitBinaryExpr(BinaryExpr*) { 
-      // TODO: Numeric binary operations (+ - * / % ** && || < <= > >= ==)
-      // TODO: Assignements (will need "AssignExprGenerator")
-      
-      // For string operations I'll need Strings implemented in the VM.
-      // For Char operations I'll need to decide on the handling of chars.
-      fox_unimplemented_feature("BinaryExpr Bytecode Generation");
+    RegisterValue visitBinaryExpr(BinaryExpr* expr) { 
+      if(expr->isAssignement())
+        fox_unimplemented_feature("Assignement BinaryExpr BCGen");
+      if (expr->getType()->isNumeric())
+        return genNumericBinaryExpr(expr);
+      fox_unimplemented_feature("Non-numeric BinaryExpr BCGen");
     }
 
     RegisterValue visitCastExpr(CastExpr*) { 
       // TODO: Numeric casts
       // For the rest we need other things in the VM.
-      fox_unimplemented_feature("CastExpr Bytecode Generation");
+      fox_unimplemented_feature("CastExpr BCGen");
     }
 
     RegisterValue visitUnaryExpr(UnaryExpr*) { 
       // TODO
-      fox_unimplemented_feature("UnaryExpr Bytecode Generation");
+      fox_unimplemented_feature("UnaryExpr BCGen");
     }
 
     RegisterValue visitArraySubscriptExpr(ArraySubscriptExpr*) { 
       // Needs Arrays implemented in the VM.
-      fox_unimplemented_feature("ArraySubscriptExpr Bytecode Generation");
+      fox_unimplemented_feature("ArraySubscriptExpr BCGen");
     }
 
     RegisterValue visitMemberOfExpr(MemberOfExpr*) { 
       // Unused for now.
-      fox_unimplemented_feature("MemberOfExpr Bytecode Generation");
+      fox_unimplemented_feature("MemberOfExpr BCGen");
     }
 
     RegisterValue visitDeclRefExpr(DeclRefExpr*) { 
@@ -104,12 +214,12 @@ class BCGen::ExprGenerator : public Generator,
       // NOTE: This won't take care of emitting DeclRefs used as
       // the LHS of an assignement. It'll be handled by another
       // visitor.
-      fox_unimplemented_feature("DeclRefExpr Bytecode Generation");
+      fox_unimplemented_feature("DeclRefExpr BCGen");
     }
 
     RegisterValue visitCallExpr(CallExpr*) { 
       // Needs functions and calls implemented in the VM.
-      fox_unimplemented_feature("CallExpr Bytecode Generation");
+      fox_unimplemented_feature("CallExpr BCGen");
     }
 
     RegisterValue visitCharLiteralExpr(CharLiteralExpr* expr) { 
@@ -129,7 +239,7 @@ class BCGen::ExprGenerator : public Generator,
     RegisterValue visitDoubleLiteralExpr(DoubleLiteralExpr*) { 
       // Needs the constant table since 16 bits floats aren't a thing
       // (= no StoreSmallFloat)
-      fox_unimplemented_feature("DoubleLiteralExpr Bytecode Generation");
+      fox_unimplemented_feature("DoubleLiteralExpr BCGen");
     }
 
     RegisterValue visitBoolLiteralExpr(BoolLiteralExpr* expr) { 
@@ -141,12 +251,12 @@ class BCGen::ExprGenerator : public Generator,
 
     RegisterValue visitStringLiteralExpr(StringLiteralExpr*) { 
       // Needs strings implemented in the VM
-      fox_unimplemented_feature("StringLiteralExpr Bytecode Generation");
+      fox_unimplemented_feature("StringLiteralExpr BCGen");
     }
 
     RegisterValue visitArrayLiteralExpr(ArrayLiteralExpr*) {
       // Needs array implemented in the VM
-      fox_unimplemented_feature("ArrayLiteralExpr Bytecode Generation");
+      fox_unimplemented_feature("ArrayLiteralExpr BCGen");
     }
 
     // ErrorExprs shouldn't be found in BCGen.
