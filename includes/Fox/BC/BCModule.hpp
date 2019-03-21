44b021c2cce536b0a6e66c3fc9ef813b21b1eb4f
Pierre-vh <pierre.vanhoutryve@outlook.com>
2019-03-21 15:57:18 +0100

    [BCModule] WIP instr_iterator

--- includes/Fox/BC/BCModule.hpp
+++ includes/Fox/BC/BCModule.hpp
@@ -15,11 +15,14 @@
 #include "Fox/BC/Instruction.hpp"
 #include "Fox/Common/LLVM.hpp"
 #include "llvm/ADT/SmallVector.h"
+#include <iterator>
 #include <iosfwd>
 
 namespace fox {
   class BCModule {
     public:
+      class instr_iterator;
+
       // Returns the number of instructions in the instruction buffer
       std::size_t numInstructions() const;
 
@@ -29,6 +32,8 @@ namespace fox {
       // Returns a constant reference to the instruction buffer
       const InstructionBuffer& getInstructionBuffer() const;
 
+      instr_iterator instrs_beg();
+
       // Dumps the module to 'out'
       void dumpModule(std::ostream& out) const;
 
@@ -36,5 +41,45 @@ namespace fox {
       // TODO: Move this out in the .cpp (using PIMPL) to reduce 
       // the number of includes.
       InstructionBuffer instrBuffer_;
+  };
+
+  // TODO: Constant iterator version + integrate it in BCModule
+  //       with 'instrs_beg' 'instrs_end' and 'instrs_back'
+  //       For the constant iterator, maybe use a common template base.
+
+  // An iterator to an instruction in a BCModule. 
+  //
+  // This wraps a BCModule* + an index because traditional vector iterators
+  // cannot be used safely due to potential reallocations.
+  class BCModule::instr_iterator {
+    // Use a 32 bit unsigned int as index type to save a bit of space.
+    using idx_type = std::uint32_t;
+    public:
+      using iterator_category = std::random_access_iterator_tag;
+      using value_type = Instruction;
+      using reference_type = Instruction&;
+      using difference_type = idx_type;
+
+      // Pre-increment
+      instr_iterator& operator++();
+      // Post-increment
+      instr_iterator operator++(int);
+
+      reference_type operator*() const;
+      reference_type operator->() const; 
+
+      friend bool operator==(instr_iterator lhs, instr_iterator rhs);
+      friend bool operator!=(instr_iterator lhs, instr_iterator rhs);
+
+    private:
+      friend class BCModuleBuilder;
+
+      reference_type& get();
+      const reference_type& get() const;
+      
+      instr_iterator(BCModule& bcModule, idx_type idx);
+
+      BCModule& bcModule_;
+      idx_type idx_;
   };
 }
--- lib/BC/BCModule.cpp
+++ lib/BC/BCModule.cpp
@@ -25,3 +25,59 @@ const InstructionBuffer& BCModule::getInstructionBuffer() const {
 void BCModule::dumpModule(std::ostream& out) const {
   dumpInstructions(out, getInstructionBuffer());
 }
+
+//----------------------------------------------------------------------------//
+// BCModule::instr_iterator
+//----------------------------------------------------------------------------//
+
+BCModule::instr_iterator& 
+BCModule::instr_iterator::operator++() {
+  idx_++;
+  return *this;
+}
+
+BCModule::instr_iterator
+BCModule::instr_iterator::operator++(int) {
+  auto save = (*this);
+  ++(*this);
+  return save;
+}
+
+BCModule::instr_iterator::reference_type 
+BCModule::instr_iterator::operator*() const {
+  return get();
+}
+
+BCModule::instr_iterator::reference_type 
+BCModule::instr_iterator::operator->() const {
+  return get();
+}
+
+bool fox::operator==(BCModule::instr_iterator lhs, 
+                     BCModule::instr_iterator rhs){
+  // Check if the modules are the same (same instance)
+  if(&(lhs.bcModule_) == &(rhs.bcModule_))
+    return lhs.idx_ == rhs.idx_;
+  return false;
+}
+
+bool fox::operator!=(BCModule::instr_iterator lhs, 
+                     BCModule::instr_iterator rhs){
+  return !(lhs == rhs);
+}
+
+BCModule::instr_iterator::reference_type
+BCModule::instr_iterator::get() {
+  auto& iBuff = bcModule_.instrBuffer_;
+  assert(idx_ < iBuff.size() 
+    && "Dereferencing past-the-end iterator");
+  return iBuff[idx_];
+}
+
+const BCModule::instr_iterator::reference_type 
+BCModule::instr_iterator::get() const {
+  return const_cast<instr_iterator*>(this)->get();
+}
+
+BCModule::instr_iterator::instr_iterator(BCModule& bcModule, idx_type idx)
+  : bcModule_(bcModule), idx_(idx) {}
