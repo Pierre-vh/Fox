55aac375eefd94b7d56dc732eb8b40ae84afa07e
Pierre van Houtryve <pierre.vanhoutryve@outlook.com>
2019-03-22 14:10:36 +0100

    Adding CondtionStmt BCGen logic

--- lib/BCGen/BCGenStmt.cpp
+++ lib/BCGen/BCGenStmt.cpp
@@ -6,8 +6,10 @@
 //----------------------------------------------------------------------------//
 
 #include "Fox/BCGen/BCGen.hpp"
+#include "Registers.hpp"
 #include "Fox/AST/Stmt.hpp"
 #include "Fox/AST/ASTVisitor.hpp"
+#include "Fox/BC/BCBuilder.hpp"
 #include "Fox/Common/Errors.hpp"
 
 using namespace fox;
@@ -32,6 +34,15 @@ class BCGen::StmtGenerator : public Generator,
     RegisterAllocator& regAlloc;
 
   private:
+    using jump_offset_t = decltype(Instruction::Jump.arg);
+    using condjump_offset_t = decltype(Instruction::CondJump.arg1);
+    
+    static constexpr jump_offset_t 
+    max_jump_offset = std::numeric_limits<jump_offset_t>::max();
+
+    static constexpr jump_offset_t 
+    min_jump_offset = std::numeric_limits<jump_offset_t>::min();
+
     //------------------------------------------------------------------------//
     // "emit" and "gen" methods 
     // 
@@ -50,6 +61,39 @@ class BCGen::StmtGenerator : public Generator,
         fox_unreachable("Unknown ASTNode kind");
     }
 
+    // This calculates the offset needed to jump to the first
+    // instruction after 'last'. It also checks that the jump
+    // offset isn't too large.
+    jump_offset_t calculateJumpOffset(BCModule::instr_iterator first,
+                                      BCModule::instr_iterator last) {
+      if(first == last) 
+        return 0;
+
+      bool isNegative = false;
+      if (last < first) {
+        std::swap(first, last);
+        isNegative = true;
+      }
+
+      // Calculate the distance between the first and the last iterator
+      auto diff = distance(first, last);
+
+      // TODO: Replace this assertion by a proper 'fatal' diagnostic explaining
+      // the problem. Maybe pass a lambda 'onOutOfRange' as parameter to
+      // this function and call onOutOfRange() + return 0; when we try to
+      // jump too far.
+      assert((diff <= static_cast<std::size_t>(max_jump_offset))
+        && "Jump is out of range!");
+
+      // Convert it to jump_offset_t now that we know that it's safe.
+      auto offset = static_cast<jump_offset_t>(diff);
+
+      assert((offset != 0) && "offset cannot be zero");
+
+      // Return, applying the minus if needed
+      return isNegative ? (-offset) : offset;
+    }
+
     //------------------------------------------------------------------------//
     // "visit" methods 
     // 
@@ -63,8 +107,51 @@ class BCGen::StmtGenerator : public Generator,
         genNode(node);
     }
 
-    void visitConditionStmt(ConditionStmt*) {
-      fox_unimplemented_feature("ConditionStmt BCGen");
+    void visitConditionStmt(ConditionStmt* stmt) {
+      BCModule& theModule = builder.getModule();
+
+      {
+        // First, generate the condition
+        RegisterValue condReg = 
+          bcGen.genExpr(builder, regAlloc, stmt->getCond());
+
+        // Then generate a condjump. This condjump will, if true, skip the jump
+        // to the else, so we'll just jump 1 instruction (since jump offsets
+        // are relative to the next instruction)
+        builder.createCondJumpInstr(condReg.getAddress(), 1);
+      }
+
+      // Now, create a jump to the code that needs to be executed
+      // if the condition isn't satisfied.
+      auto jumpIfNot = builder.createJumpInstr(0);
+
+      // Compile the 'then'
+      visit(stmt->getThen());
+
+      auto lastThenInstr = theModule.instrs_back();
+
+      // Compile the 'else' if present
+      if (CompoundStmt* elseBody = stmt->getElse()) {
+        // Create a jump to the end of the condition (same as 'jumpToElse')
+        // so the if's code doesn't fall-through and executes the else too.
+        auto jumpEnd = builder.createJumpInstr(0);
+        // Now we can complete 'jumpIfNot' so it executes the else.
+        jumpIfNot->Jump.arg = calculateJumpOffset(jumpIfNot, jumpEnd);
+        // Compile the else
+        visit(elseBody);
+        // Complete 'jumpEnd' so it jumps to the last instruction emitted.
+        jumpEnd->Jump.arg = calculateJumpOffset(jumpEnd, 
+                                                theModule.instrs_back());
+      }
+      // No 'else'
+      else {
+        // Just complete 'jumpToElse' to jump after the last instruction
+        // emitted.
+        // FIXME: Is this safe? We'll probably have instruction be generated
+        // after (at least a return) but it can't be guaranteed here.
+        jumpIfNot->Jump.arg = calculateJumpOffset(jumpIfNot, 
+                                                  theModule.instrs_back());
+      }
     }
 
     void visitWhileStmt(WhileStmt*) {
