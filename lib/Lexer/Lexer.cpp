735cb378186c6d2bdb418b1fc5c4d3e807b6f6d3
Pierre van Houtryve <pierre.vanhoutryve@outlook.com>
2019-03-26 17:50:35 +0100

    Merging Token/Lexer.cpp

--- lib/Lexer/Lexer.cpp
+++ lib/Lexer/Lexer.cpp
@@ -4,8 +4,11 @@
 // File : Lexer.cpp                      
 // Author : Pierre van Houtryve                
 //----------------------------------------------------------------------------//
+// Implements the Token and Lexer interfaces.
+//----------------------------------------------------------------------------//
 
 #include "Fox/Lexer/Lexer.hpp"
+#include "Fox/Lexer/Token.hpp"
 #include "Fox/Common/DiagnosticEngine.hpp"
 #include "Fox/Common/SourceManager.hpp"
 #include "Fox/Common/Errors.hpp"
@@ -14,6 +17,44 @@
 #include <cctype>
 
 using namespace fox;
+
+//----------------------------------------------------------------------------//
+// Token
+//----------------------------------------------------------------------------//
+
+static const char* getKindSpelling(TokenKind kind) {
+  switch (kind) {
+    #define TOKEN(ID) case TokenKind::ID: return #ID;
+    #include "Fox/Lexer/TokenKinds.def"
+    default: 
+      fox_unreachable("unhandled token kind");
+  }
+}
+
+Token::Token(Kind kind, string_view str, SourceRange range) :
+  kind(kind), str(str), range(range) {
+  assert(range && "Token constructed with invalid an SourceRange");
+}
+
+bool Token::isValid() const {
+  return kind != Kind::Invalid;
+}
+
+Token::operator bool() const {
+  return isValid();
+}
+
+bool Token::is(Kind kind) const {
+  return (this->kind == kind);
+}
+
+void Token::dump(std::ostream& out) const {
+  out << str << " [" << getKindSpelling(kind) << "]\n";
+}
+
+//----------------------------------------------------------------------------//
+// Lexer
+//----------------------------------------------------------------------------//
 
 Lexer::Lexer(ASTContext& astctxt): ctxt(astctxt), 
   diagEngine(ctxt.diagEngine), srcMgr(ctxt.sourceMgr) {}
