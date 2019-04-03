7ff2550b37d9534e981ee8f33e5e9422bdb78927
Pierre-vh <pierre.vanhoutryve@outlook.com>
2019-04-03 10:00:24 +0200

    Header fix

--- includes/Fox/Common/DiagnosticVerifier.hpp
+++ includes/Fox/Common/DiagnosticVerifier.hpp
@@ -1,6 +1,6 @@
 //----------------------------------------------------------------------------//
-// This file is a part of The Moonshot Project.
-// See the LICENSE.txt file at the root of the project for license information.
+// Part of the Fox project, licensed under the MIT license.
+// See LICENSE.txt in the project root for license information. 
 // File : DiagnosticVerifier.hpp
 // Author : Pierre van Houtryve
 //----------------------------------------------------------------------------//
