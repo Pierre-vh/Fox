38c7623ef9dacd960e0c06f76881c7c4647d25ff
Pierre van Houtryve <pierre.vanhoutryve@outlook.com>
2019-04-03 22:22:58 +0200

    [WIP] StableVectorIterator: Making the interface richer, removing useless template arguments

--- includes/Fox/Common/StableVectorIterator.hpp
+++ includes/Fox/Common/StableVectorIterator.hpp
@@ -10,7 +10,9 @@
 // TODO: 
 //    1) Finish the implementation
 //    2) Test this thouroughly
-//    3) Code review
+//      - Const and Non-Const variants
+//      - Test every functionality of the interface
+//    3) Code review/re-read.
 
 #pragma once
 
@@ -21,13 +23,13 @@ namespace fox {
     /// Traits helper class for StableVectorIteratorImpl to reduce
     /// code duplication when implementing both the const and non-const
     /// variants of the iterator.
-    template<typename ContainerTy, typename ElemTy, bool isConst>
+    template<typename ContainerTy, bool isConst>
     class StableVectorIteratorTraits {
       /* for non-const iterators */
       public:
-        using value_type          = ElemTy;
-        using reference           = ElemTy&;
-        using pointer             = ElemTy*;
+        using value_type          = typename ContainerTy::value_type;
+        using reference           = typename ContainerTy::reference;
+        using pointer             = typename ContainerTy::pointer;
         using container           = ContainerTy;
         using container_reference = ContainerTy&;
         using container_pointer   = ContainerTy*;
@@ -35,13 +37,13 @@ namespace fox {
     };
 
     /// StableVectorIteratorTraits for const iterators
-    template<typename ContainerTy, typename ElemTy>
-    class StableVectorIteratorTraits<ContainerTy, ElemTy, true> {
+    template<typename ContainerTy>
+    class StableVectorIteratorTraits<ContainerTy, true> {
       /* for const iterators */
       public:
-        using value_type          = const ElemTy;
-        using reference           = const ElemTy&;
-        using pointer             = const ElemTy*;
+        using value_type          = const typename ContainerTy::value_type;
+        using reference           = typename ContainerTy::const_reference;
+        using pointer             = typename ContainerTy::const_pointer;
         using container           = const ContainerTy;
         using container_reference = const ContainerTy&;
         using container_pointer   = const ContainerTy*;
@@ -49,19 +51,26 @@ namespace fox {
     };
   }
 
-  /// StableVectorIteratorImpl is a vector iterator that guarantees stability
-  /// in some scenarios.
-  ///   - Stability of the 'end' iterator is guaranteed
-  ///   - Stability of iterators is guaranteed when inserting/erasing AFTER the 
-  ///     iterator's position.
-  /// Stability IS NOT guaranteed for insertion/erasure before the iterator.
-  template<typename ContainerTy, typename ElemTy, bool isConst>
+  /// StableVectorIteratorImpl is a custom vector iterator with a few additions:
+  /// Stability is guaranteed for:
+  ///   - The 'end' iterator is guaranteed
+  ///   - Inserting/erasing AFTER the iterator's position.
+  /// Stability IS NOT guaranteed for
+  ///   - Insertion/erasure before the iterator.
+  /// Additional notes:
+  ///   - The iterator will assert that it's dereferenceable (that it points
+  ///     to a valid element
+  ///   - ContainerTy (the first template argument) needs to be a std::vector
+  ///     or another data structure with the same interface as a std::vector.
+  ///     (e.g. llvm::SmallVector)
+  /// TODO: This could be a RandomAccessIterator, however I don't need that
+  /// functionality for now so I haven't implemented it.
+  template<typename ContainerTy, bool isConst>
   class StableVectorIteratorImpl {
     using trait = 
-      detail::StableVectorIteratorTraits<ContainerTy, ElemTy, isConst>;
-    using this_type = StableVectorIteratorImpl<ContainerTy, ElemTy, isConst>;
+      detail::StableVectorIteratorTraits<ContainerTy, isConst>;
+    using this_type = StableVectorIteratorImpl<ContainerTy, isConst>;
     public:
-      using index_type          = std::size_t;
       using iterator_category   = std::bidirectional_iterator_tag;
       using difference_type     = std::ptrdiff_t;
       using value_type          = typename trait::value_type;
@@ -73,15 +82,14 @@ namespace fox {
       using container_iterator  = typename trait::container_iterator;
 
       /// Default Constructor
-      StableVectorIteratorImpl() {
-        // TODO
-      }
+      StableVectorIteratorImpl() = default;
 
       /// Constructor
-      StableVectorIteratorImpl(container_reference container, 
-                               index_type idx) {
-        // TODO
-      }
+      /// \param data the container
+      /// \param index the index
+      explicit StableVectorIteratorImpl(container_reference data, 
+                                        std::size_t index = 0) : 
+                                        data_(&data), index_(index) {}
 
       // Pre-increment
       this_type& operator++() {
@@ -115,7 +123,7 @@ namespace fox {
       /// (ContainerTy::iterator or ContainerTy::const_iterator, depending
       /// on if this iterator is constant or not)
       container_iterator getContainerIterator() const {
-        
+        // TODO 
       }
 
       friend bool operator==(const this_type& lhs, const this_type& rhs) {
@@ -129,13 +137,29 @@ namespace fox {
       /// Calculates the distance between 2 iterators, \p first and \p last.
       friend difference_type
       distance(const this_type& first, const this_type& last) {
-
+        // TODO
       }
 
     private:
       // bool isDereferenceable() const
-      static constexpr index_type npos = -1;
-      container_pointer container_ = nullptr;
-      index_type index_ = 0;
+      // container_reference getContainer() const
+      //  check that it's non null (else it's not dereferenceable)
+      // void moveToEnd() (sets index_ = npos)
+      // void isEnd() 
+      // bool compareContainers(const this_type& other) const
+      // pointer get() const 
+      //    Returns the pointer to the element if dereferenceable. assert that
+      //    index < container.size() (else it's not dereferenceable)
+      // void advance(difference_type t) (both positive and negative arg)
+
+      static constexpr std::size_t npos = -1;
+      container_pointer data_ = nullptr;
+      std::size_t index_ = 0;
   };
+
+  template<typename Container>
+  using StableVectorIterator = StableVectorIteratorImpl<Container, false>;
+
+  template<typename Container>
+  using StableVectorConstIterator = StableVectorIteratorImpl<Container, true>;
 }
