c52cfbc57a1c69d49b5679d14da747d2af097b14
Pierre van Houtryve <pierre.vanhoutryve@outlook.com>
2019-04-29 21:56:44 +0200

    Moving BuiltinsTest.cpp to ASTTests.cpp

--- unittests/src/Fox/AST/ASTTests.cpp
+++ unittests/src/Fox/AST/ASTTests.cpp
@@ -8,9 +8,10 @@
 //----------------------------------------------------------------------------//
 
 #include "gtest/gtest.h"
+#include "Fox/AST/ASTContext.hpp"
+#include "Fox/AST/BuiltinsTypes.hpp"
 #include "Fox/AST/Decl.hpp"
 #include "Fox/AST/Expr.hpp"
-#include "Fox/AST/ASTContext.hpp"
 #include "Fox/AST/Types.hpp"
 #include "Fox/AST/ASTVisitor.hpp"
 #include "Fox/AST/TypeVisitor.hpp"
@@ -540,4 +541,54 @@ TEST_F(ASTTest, functionTypesUniqueness) {
   FunctionTypeParam p2immut(boolTy, false);
   EXPECT_NE(FunctionType::get(ctxt, {p2immut}, voidTy), 
             FunctionType::get(ctxt, {p2}, voidTy));
+}
+
+class BuiltinsTest : public ::testing::Test {
+  public:
+    BuiltinsTest() : diags(srcMgr, std::cout), ctxt(srcMgr, diags) {}
+
+    SourceManager srcMgr;
+    DiagnosticEngine diags;
+    ASTContext ctxt;
+
+};
+
+TEST_F(BuiltinsTest, getTypeOfBuiltinTest) {
+  Type printIntTy = getTypeOfBuiltin(ctxt, BuiltinID::printInt);
+  Type printBoolTy = getTypeOfBuiltin(ctxt, BuiltinID::printBool);
+
+  // Is 'toDebugString' fine for this?
+  EXPECT_EQ(printIntTy->toDebugString(),  "(int) -> void");
+  EXPECT_EQ(printBoolTy->toDebugString(), "(bool) -> void");
+}
+
+TEST_F(BuiltinsTest, builtinIdentifier) {
+  EXPECT_EQ(ctxt.getIdentifier(BuiltinID::printBool).getStr(), 
+            "printBool");
+  EXPECT_EQ(ctxt.getIdentifier(BuiltinID::printBool),
+            ctxt.getIdentifier(BuiltinID::printBool));
+
+  EXPECT_EQ(ctxt.getIdentifier(BuiltinID::printInt).getStr(), 
+            "printInt");
+  EXPECT_EQ(ctxt.getIdentifier(BuiltinID::printInt),
+            ctxt.getIdentifier(BuiltinID::printInt));
+}
+
+TEST_F(BuiltinsTest, builtinLookup) {
+  auto printBool = ctxt.getIdentifier(BuiltinID::printBool);
+  auto printInt = ctxt.getIdentifier(BuiltinID::printInt);
+  {
+    SmallVector<BuiltinFuncDecl*, 4> results;
+    ctxt.lookupBuiltin(printBool, results);
+    ASSERT_EQ(results.size(), 1u) 
+      << "Incorrect number of results for " << printBool.getStr();
+    EXPECT_EQ(results.front(), BuiltinFuncDecl::get(ctxt, BuiltinID::printBool));
+  }
+  {
+    SmallVector<BuiltinFuncDecl*, 4> results;
+    ctxt.lookupBuiltin(printInt, results);
+    ASSERT_EQ(results.size(), 1u) 
+      << "Incorrect number of results for " << printInt.getStr();
+    EXPECT_EQ(results.front(), BuiltinFuncDecl::get(ctxt, BuiltinID::printInt));
+  }
 }
