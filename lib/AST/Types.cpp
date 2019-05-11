f33fa4dfbfdfd7806f00bf7c2ab9c36aca02067b
Pierre van Houtryve <pierre.vanhoutryve@outlook.com>
2019-05-11 12:16:54 +0200

    Merge Type.cpp into Types.cpp

--- lib/AST/Types.cpp
+++ lib/AST/Types.cpp
@@ -6,6 +6,7 @@
 //----------------------------------------------------------------------------//
 
 #include "Fox/AST/Types.hpp"
+#include "Fox/AST/Type.hpp"
 #include "Fox/Common/Errors.hpp"
 #include "Fox/AST/ASTContext.hpp"
 #include "Fox/AST/ASTVisitor.hpp"
@@ -20,6 +21,96 @@ using namespace fox;
   static_assert(std::is_trivially_destructible<ID>::value, \
   #ID " is allocated in the ASTContext: Its destructor is never called!");
 #include "Fox/AST/TypeNodes.def"
+
+//----------------------------------------------------------------------------//
+// Type
+//----------------------------------------------------------------------------//
+
+Type::Type(TypeBase* ty) :
+  ty_(ty) {}
+
+TypeBase* Type::getPtr() {
+  return ty_;
+}
+
+const TypeBase* Type::getPtr() const {
+  return ty_;
+}
+
+bool Type::isNull() const {
+  return (ty_ == nullptr);
+}
+
+TypeBase* Type::operator->() {
+  assert(ty_ && "Cannot use this if the pointer is null.");
+  return ty_;
+}
+
+const TypeBase* Type::operator->() const {
+  assert(ty_ && "Cannot use this if the pointer is null.");
+  return ty_;
+}
+
+Type::operator bool() const {
+  return !isNull();
+}
+
+bool Type::operator==(const Type& type) const {
+  return ty_ == type.ty_;
+}
+
+bool Type::operator!=(const Type& type) const {
+  return !(*this == type);
+}
+
+bool Type::operator<(const Type other) const {
+  return getPtr() < other.getPtr();
+}
+
+std::ostream& fox::operator<<(std::ostream& os, Type ty) {
+  assert(ty && "can't print a null type");
+  os << ty->toString();
+  return os;
+}
+
+//----------------------------------------------------------------------------//
+// TypeLoc
+//----------------------------------------------------------------------------//
+
+TypeLoc::TypeLoc(Type ty, SourceRange range) :
+  ty_(ty), range_(range) {}
+
+SourceRange TypeLoc::getSourceRange() const {
+  return range_;
+}
+
+SourceLoc TypeLoc::getBeginLoc() const {
+  return range_.getBeginLoc();
+}
+
+SourceLoc TypeLoc::getEndLoc() const {
+  return range_.getEndLoc();
+}
+
+bool TypeLoc::isLocValid() const {
+  return (bool)range_;
+}
+
+bool TypeLoc::isTypeValid() const {
+  return (bool)ty_;
+}
+
+bool TypeLoc::isComplete() const {
+  return isLocValid() && isTypeValid();
+}
+
+Type TypeLoc::getType() {
+  return ty_;
+}
+
+const Type TypeLoc::getType() const {
+  return ty_;
+}
 
 //----------------------------------------------------------------------------//
 // TypePrinter
