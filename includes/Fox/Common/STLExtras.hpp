64c5a0ff854038eb50a8352834af9b4d6e077265
Pierre van Houtryve <pierre.vanhoutryve@outlook.com>
2019-05-17 23:27:51 +0200

    Refactor STLExtras

--- includes/Fox/Common/STLExtras.hpp
+++ includes/Fox/Common/STLExtras.hpp
@@ -15,20 +15,20 @@
 namespace fox {
   namespace detail {
     template <class F, class Tuple, std::size_t... I>
-    constexpr decltype(auto) apply_impl(F&& f, Tuple&& t, std::index_sequence<I...>)
-    {
-        return std::invoke(std::forward<F>(f), 
-                           std::get<I>(std::forward<Tuple>(t))...);
+    constexpr decltype(auto) 
+    apply_impl(F&& f, Tuple&& t, std::index_sequence<I...>) {
+      return std::invoke(std::forward<F>(f), 
+        std::get<I>(std::forward<Tuple>(t))...);
     }
   }  // namespace detail
  
   template <class F, class Tuple>
-  constexpr decltype(auto) apply(F&& f, Tuple&& t)
-  {
-      return detail::apply_impl(
-          std::forward<F>(f), std::forward<Tuple>(t),
-          std::make_index_sequence<
-            std::tuple_size_v<
-              std::remove_reference_t<Tuple>>>{});
+  constexpr decltype(auto) apply(F&& f, Tuple&& t) {
+    return detail::apply_impl(
+      std::forward<F>(f), 
+      std::forward<Tuple>(t),
+      std::make_index_sequence<
+        std::tuple_size_v<std::remove_reference_t<Tuple>>>{}
+    );
   }
 }
