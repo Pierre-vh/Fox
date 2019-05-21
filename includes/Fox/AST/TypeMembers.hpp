8fc0dfe752e8860dbc832e93b5fcb3a0ddda165a
Pierre van Houtryve <pierre.vanhoutryve@outlook.com>
2019-05-21 17:11:08 +0200

    Renaming TypeBuiltins to TypeMembers

--- includes/Fox/AST/TypeMembers.hpp
+++ includes/Fox/AST/TypeMembers.hpp
@@ -1,7 +1,7 @@
 //----------------------------------------------------------------------------//
 // Part of the Fox project, licensed under the MIT license.
 // See LICENSE.txt in the project root for license information.     
-// File : TypeBuiltins.hpp                      
+// File : TypeMembers.hpp                      
 // Author : Pierre van Houtryve                
 //----------------------------------------------------------------------------//
 // Contains the "TypeBuiltinKind" enum and related functions.
@@ -14,21 +14,21 @@
 #include <iosfwd>
 
 namespace fox {
-  /// Kinds of members of builtin types.
-  enum class TypeBuiltinKind : std::uint16_t {
-    #define TYPE_BUILTIN_METHOD(ID, FOX) ID,
-    #define TYPE_BUILTIN_RANGE(ID, FIRST, LAST)\
+  /// ID of members of builtin types.
+  enum class TypeMemberKind : std::uint16_t {
+    #define ANY_MEMBER(ID) ID,
+    #define MEMBER_RANGE(ID, FIRST, LAST)\
       First_##ID = FIRST, Last_##ID = LAST,
-    #include "TypeBuiltins.def"
+    #include "TypeMembers.def"
   };
 
   /// \returns true if \p kind is a kind of String builtin
-  bool isStringBuiltin(TypeBuiltinKind kind) {
-    return (kind >= TypeBuiltinKind::First_StringBuiltin) &&
-           (kind <= TypeBuiltinKind::Last_StringBuiltin);
+  bool isStringBuiltin(TypeMemberKind kind) {
+    return (kind >= TypeMemberKind::First_StringMember) &&
+           (kind <= TypeMemberKind::Last_StringMember);
   }
 
   /// Converts a TypeBuiltinKind to a human-readable string.
-  const char* to_string(TypeBuiltinKind value);
-  std::ostream& operator<<(std::ostream& os, TypeBuiltinKind value);
+  const char* to_string(TypeMemberKind value);
+  std::ostream& operator<<(std::ostream& os, TypeMemberKind value);
 }
--- lib/AST/TypeMembers.cpp
+++ lib/AST/TypeMembers.cpp
@@ -1,25 +1,25 @@
 //----------------------------------------------------------------------------//
 // Part of the Fox project, licensed under the MIT license.
 // See LICENSE.txt in the project root for license information.      
-// File : TypeBuiltins.cpp                      
+// File : TypeMembers.cpp                      
 // Author : Pierre van Houtryve                
 //----------------------------------------------------------------------------//
 
-#include "Fox/AST/TypeBuiltins.hpp"
+#include "Fox/AST/TypeMembers.hpp"
 #include "Fox/Common/Errors.hpp"
 
 using namespace fox;
 
-const char* fox::to_string(TypeBuiltinKind value) {
+const char* fox::to_string(TypeMemberKind value) {
   switch (value) {
-    #define TYPE_BUILTIN_METHOD(ID, FOX)\
-      case TypeBuiltinKind::ID: return #ID;
-    #include "Fox/AST/TypeBuiltins.def"
+    #define ANY_MEMBER(ID)\
+      case TypeMemberKind::ID: return #ID;
+    #include "Fox/AST/TypeMembers.def"
     default:
       fox_unreachable("unknown TypeBuiltinKind");
   }
 }
 
-std::ostream& fox::operator<<(std::ostream& os, TypeBuiltinKind value) {
+std::ostream& fox::operator<<(std::ostream& os, TypeMemberKind value) {
   return os << to_string(value);
 }
