45ab617e987b358d96da282cefc05930519035da
Pierre-vh <pierre.vanhoutryve@outlook.com>
2019-06-20 14:03:41 +0300

    [Fix] Use SourceRange() not FileID()

--- lib/VM/VMDiagnostics.cpp
+++ lib/VM/VMDiagnostics.cpp
@@ -16,10 +16,12 @@ Diagnostic VM::diagnose(DiagID diag) const {
   // TO-DO: Use DebugInfo for the current instruction to emit a more precise
   // diagnostic
 
-  return diagEngine.report(diag, FileID());
+  return diagEngine.report(diag, SourceRange());
   
   // FIXME: Sometimes we might want to emit notes after the diag is emitted.
   // To do that we'd need to have a "callback" function that is called once
   // the diagnostic has been emitted. This needs modification to the 
   // Diagnostic object.
+
+  // TO-DO: Stop execution
 }
